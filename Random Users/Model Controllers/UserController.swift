91a6e5fc869c7e81f04d53d7662906a6edab4425
Conner Alegre <cga0@pm.me>
2018-09-07 10:26:44 -0600

    load users name with images into tableview

--- Random Users/Model Controllers/UserController.swift
+++ Random Users/Model Controllers/UserController.swift
@@ -9,5 +9,32 @@
 import Foundation
 
 class UserController {
-    
+    func getUsers(completion: @escaping (Error?) -> Void) {
+        let url = UserController.baseURL
+        
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            if let error = error {
+                NSLog("Error GETting randomUser data: \(error) - \(url)")
+                completion(error)
+                return
+            }
+
+            guard let data = data else { return }
+
+            do {
+                let decoder = JSONDecoder()
+                let randomUserJSON = try decoder.decode(Results.self, from: data)
+        
+                self.users = randomUserJSON.results.compactMap { $0 }
+            } catch let error {
+                NSLog("Error decoding data: \(error)")
+                completion(error)
+                return
+            }
+            completion(nil)
+        }.resume()
+    }
+
+    var users: [User] = []
+    static var baseURL = URL(string: "https://randomuser.me/api/?format=json&inc=name,email,phone,picture&results=1000")!
 }
--- Random Users/Model/User.swift
+++ Random Users/Model/User.swift
@@ -8,21 +8,25 @@
 
 import Foundation
 
-struct User {
+struct Results: Codable {
+    let results: [User]
+}
+
+struct User: Codable {
     let name: Name
     let email: String
     let phone: String
     let picture: Picture
-}
-
-struct Name {
-    let title: String
-    let first: String
-    let last: String
-}
-
-struct Picture {
-    let large: String
-    let medium: String
-    let thumbnail: String
+    
+    struct Name: Codable {
+        let title: String
+        let first: String
+        let last: String
+    }
+    
+    struct Picture: Codable {
+        let large: String
+        let medium: String
+        let thumbnail: String
+    }
 }
