ece4c790a5292446c52d0891c3c5da3592f96ef9
stdemicheli <stdemicheli@gmail.com>
2018-09-07 17:29:44 +0200

    Added User model with manual decoding.

--- Random Users/Models/User.swift
+++ Random Users/Models/User.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+struct User: Decodable, Equatable {
+    
+    var name: String
+    var gender: String
+    var phone: String
+    var email: String
+    var pictureUrl: URL
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case gender
+        case phone
+        case email
+        case image
+        case pictureUrl = "picture"
+        
+        enum PictureCodingKeys: String, CodingKey {
+            case medium
+        }
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.gender = try container.decode(String.self, forKey: .gender)
+        self.phone = try container.decode(String.self, forKey: .gender)
+        self.email = try container.decode(String.self, forKey: .email)
+        
+        let pictureContainer = try container.nestedContainer(keyedBy: CodingKeys.PictureCodingKeys.self, forKey: .pictureUrl)
+        self.pictureUrl = try pictureContainer.decode(URL.self, forKey: .medium)
+    }
+    
+}
