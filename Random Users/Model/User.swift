bc35597a7ba6478c40d753fddd3cd4d223751226
Carolyn Lea <carolynrlea@gmail.com>
2018-09-07 10:35:20 -0600

    add User

--- Random Users/Model/User.swift
+++ Random Users/Model/User.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+
+struct Name: Codable
+{
+    var title: String
+    var firstName: String
+    var lastName: String
+    
+    enum NameCodingKeys: String, CodingKey
+    {
+        case title
+        case first
+        case last
+    }
+}
+
+
+struct User: Codable
+{
+    var nameComponents: [Name]
+    var thumbnail: Data
+    var image: Data
+    var email: String
+    var phone: String
+    var identifier: [String:String]
+    
+    enum CodingKeys: String, CodingKey
+    {
+        case nameComponents = "name"
+        case thumbnail
+        case image = "large"
+        case email
+        case phone
+        case identifier = "id"
+        
+        enum idCodingKeys: String, CodingKey
+        {
+            case name
+            case value
+        }
+    }
+}
+
+
+
