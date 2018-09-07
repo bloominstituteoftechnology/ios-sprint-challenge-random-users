d1a7f78beb52970ad8fd06c6f6365fb606eb46de
Carolyn Lea <carolynrlea@gmail.com>
2018-09-07 11:08:52 -0600

    add userfetchoperation

--- Random Users/ModelController/UserController.swift
+++ Random Users/ModelController/UserController.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+
+class UserController
+{
+    let baseURL = URL(string: "https://randomuser.me/api/?format=json&inc=name,email,phone,picture&results=1000")!
+    
+    
+    
+///////end
+}
