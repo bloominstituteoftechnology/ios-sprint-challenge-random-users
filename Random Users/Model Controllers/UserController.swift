772bf443130d2c820c83b731b36b0fef3a71249a
Conner Alegre <cga0@pm.me>
2018-09-07 09:28:41 -0600

    add user/model controller

---
+++ Random Users/Model Controllers/UserController.swift
@@ -0,0 +1,13 @@
+//
+//  UserController.swift
+//  Random Users
+//
+//  Created by Conner on 9/7/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+class UserController {
+    
+}
