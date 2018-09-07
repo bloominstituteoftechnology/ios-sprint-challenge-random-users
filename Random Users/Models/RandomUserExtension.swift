098dba0fbda0931a9a0844dcf43855051248a529
Lisa Sampson <lisasampson0918@gmail.com>
2018-09-07 14:09:36 -0400

    add load image method

--- Random Users/Models/RandomUserExtension.swift
+++ Random Users/Models/RandomUserExtension.swift
@@ -10,6 +10,11 @@ import UIKit
 
 extension RandomUser: Decodable {
     
+    enum Images: String {
+        case thumbnail
+        case large
+    }
+    
     enum CodingKeys: String, CodingKey {
         case name
         case phoneNumber = "phone"
