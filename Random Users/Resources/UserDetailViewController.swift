bb74dd7a65d60c792e9eb9de830c8bd9ef986050
Daniela Parra <dyparra47@gmail.com>
2018-10-12 11:16:16 -0400

    Set up storyboard and basic cocoa touch files.

---
+++ Random Users/Resources/UserDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserDetailViewController.swift
+//  Random Users
+//
+//  Created by Daniela Parra on 10/12/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func updateViews() {
+        
+    }
+    
+    // MARK: - Properties
+    
+    //var person
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+}
---
+++ Random Users/Resources/UsersTableViewController.swift
@@ -0,0 +1,48 @@
+//
+//  UsersTableViewController.swift
+//  Random Users
+//
+//  Created by Daniela Parra on 10/12/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UsersTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+  
+
+     
+     
+     @IBAction func addUsers(_ sender: Any) {
+     }
+ 
+    // MARK: - Navigation
+
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+}
