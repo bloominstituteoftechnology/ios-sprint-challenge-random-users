82ca558d4aa90c87fb3d645c3cfa070322636368
Dillon McElhinney <dillon.mce@gmail.com>
2018-10-12 10:00:25 -0600

    Build out contact model and model controller.

---
+++ Random Users/ContactController.swift
@@ -0,0 +1,57 @@
+//
+//  ContactController.swift
+//  Random Users
+//
+//  Created by Dillon McElhinney on 10/12/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+class ContactController {
+    private(set) var contacts: [Contact] = []
+    private let baseURL = URL(string: "https://randomuser.me/api/")!
+    
+    func fetchContacts(completion: @escaping CompletionHandler = { _ in }) {
+        guard var requestComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true) else {
+            completion(NSError())
+            return
+        }
+        let formatQuery = URLQueryItem(name: "format", value: "json")
+        let includeQuery = URLQueryItem(name: "inc", value: "name,email,phone,picture")
+        let resultsQuery = URLQueryItem(name: "results", value: "1000")
+        
+        requestComponents.queryItems = [formatQuery, includeQuery, resultsQuery]
+        
+        guard let url = requestComponents.url else {
+            NSLog("Couldn't make url from components: \(requestComponents)")
+            completion(NSError())
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            if let error = error {
+                NSLog("Error GETting contacts: \(error)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data was returned.")
+                completion(NSError())
+                return
+            }
+            
+            do {
+                let contactResults = try JSONDecoder().decode(ContactResults.self, from: data)
+                self.contacts = contactResults.results
+                completion(nil)
+            } catch {
+                NSLog("Error decoding contacts: \(error)")
+                completion(error)
+                return
+            }
+        }.resume()
+    }
+    
+}
---
+++ Random Users/Model/Contact.swift
@@ -0,0 +1,80 @@
+//
+//  Contact.swift
+//  Random Users
+//
+//  Created by Dillon McElhinney on 10/12/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+struct Contact: Decodable {
+    
+    // MARK: - Properties
+    let title: String
+    let firstName: String
+    let lastName: String
+    let email: String
+    let phoneNumbers: [String]
+    let thumbnailURL: URL?
+    let imageURL: URL?
+    
+    var name: String {
+        return "\(title.capitalized) \(firstName.capitalized) \(lastName.capitalized)"
+    }
+    
+    // MARK: - Decodable
+    enum CodingKeys: String, CodingKey {
+        case name
+        case email
+        case phone
+        case cell
+        case picture
+        
+        enum NameCodingKeys: String, CodingKey {
+            case title
+            case first
+            case last
+        }
+        
+        enum PictureCodingKeys: String, CodingKey {
+            case large
+            case thumbnail
+        }
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let email = try container.decode(String.self, forKey: .email)
+        let phone = try container.decodeIfPresent(String.self, forKey: .phone)
+        let cell = try container.decodeIfPresent(String.self, forKey: .cell)
+        var phoneNumbers: [String] = []
+        if let phone = phone { phoneNumbers.append(phone) }
+        if let cell = cell { phoneNumbers.append(cell) }
+        
+        let nameContainer = try container.nestedContainer(keyedBy: CodingKeys.NameCodingKeys.self, forKey: .name)
+        let title = try nameContainer.decode(String.self, forKey: .title)
+        let firstName = try nameContainer.decode(String.self, forKey: .first)
+        let lastName = try nameContainer.decode(String.self, forKey: .last)
+        
+        let pictureContainer = try container.nestedContainer(keyedBy: CodingKeys.PictureCodingKeys.self, forKey: .picture)
+        let thumbnailURLString = try pictureContainer.decode(String.self, forKey: .thumbnail)
+        let thumbnailURL = URL(string: thumbnailURLString)
+        
+        let imageURLString = try pictureContainer.decode(String.self, forKey: .large)
+        let imageURL = URL(string: imageURLString)
+        
+        self.title = title
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.phoneNumbers = phoneNumbers
+        self.thumbnailURL = thumbnailURL
+        self.imageURL = imageURL
+    }
+}
+
+struct ContactResults: Decodable {
+    let results: [Contact]
+}
