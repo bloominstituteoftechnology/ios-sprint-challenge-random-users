70dc7f571f0f3daaac90008ee1739830798ed2fc
Jonah Bergevin <jonahbergevin@gmail.com>
2018-10-12 10:46:45 -0600

    Trying to do this more often. First Commit

---
+++ Random Users/Model/UserController.swift
@@ -0,0 +1,9 @@
+//
+//  UserController.swift
+//  Random Users
+//
+//  Created by Madison Waters on 10/12/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import Foundation
---
+++ Random Users/View Controller/UsersTableViewController.swift
@@ -0,0 +1,57 @@
+//
+//  UsersTableViewController.swift
+//  Random Users
+//
+//  Created by Madison Waters on 10/12/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UsersTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+    // Used for Cancel capability // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    
+    // MARK: - Navigation // UserToDetail
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
