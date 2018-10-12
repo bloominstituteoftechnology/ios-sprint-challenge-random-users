a213303bc01950c23c154004f15678558047e6c8
iyinraphael <iyinraphael@gmail.com>
2018-10-12 08:50:21 -0700

    storyboard setUp

---
+++ Random Users/View Controller/RandomUserTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  RandomUserTableViewCell.swift
+//  Random Users
+//
+//  Created by Iyin Raphael on 10/12/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class RandomUserTableViewCell: UITableViewCell {
+
+   
+    
+    
+    @IBOutlet weak var randomImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
---
+++ Random Users/View Controller/RandomUserViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RandomUserViewController.swift
+//  Random Users
+//
+//  Created by Iyin Raphael on 10/12/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class RandomUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var randomImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    
+    @IBOutlet weak var emailAddressLabel: UILabel!
+}
