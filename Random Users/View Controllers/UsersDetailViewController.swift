7e5ef919bf972b336da5e2500ff17d52dbf8c314
Scott Bennett <stt.btt@gmail.com>
2018-10-12 11:31:48 -0400

    Basic UI setup

---
+++ Random Users/View Controllers/UsersDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  UsersDetailViewController.swift
+//  Random Users
+//
+//  Created by Scott Bennett on 10/12/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UsersDetailViewController: UIViewController {
+
+    @IBOutlet weak var userImageview: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userPhoneLabel: UILabel!
+    @IBOutlet weak var userEmailLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+
+}
---
+++ Random Users/View Controllers/UsersTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  UsersTableViewCell.swift
+//  Random Users
+//
+//  Created by Scott Bennett on 10/12/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UsersTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+
+
+    @IBAction func addUsersButton(_ sender: Any) {
+    }
+}
