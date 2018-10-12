cfadcb7c65beee0bdeb05fc3bb91541679b88ef3
Dillon McElhinney <dillon.mce@gmail.com>
2018-10-12 09:18:48 -0600

    Lay out views and add view controllers.

---
+++ Random Users/View Controller/ContactDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ContactDetailViewController.swift
+//  Random Users
+//
+//  Created by Dillon McElhinney on 10/12/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class ContactDetailViewController: UIViewController {
+
+    // MARK: - Properties
+    @IBOutlet weak var contactImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    
+    // MARK: - Lifecycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
