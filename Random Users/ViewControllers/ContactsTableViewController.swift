64216889a7e55ac7c67ba9ee80c8e223236edb6d
Farhan Farooqui <farhan@farooqui.net>
2018-10-12 11:34:26 -0400

    Generate button implemented

--- Random Users/ViewControllers/ContactsTableViewController.swift
+++ Random Users/ViewControllers/ContactsTableViewController.swift
@@ -12,13 +12,13 @@ class ContactsTableViewController: UITableViewController {
     
     // MARK:- Properties
     
-    
+    let users = UsersController().getUsers()
     
     // MARL:- Table View Delegate Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.isHidden = true
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -29,62 +29,27 @@ class ContactsTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     @IBAction func generateUsers(_ sender: Any) {
-    }
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        tableView.isHidden = false
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        guard let users = users?.users else {fatalError()}
+        return users.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath)
 
         // Configure the cell...
+        guard let users = users?.users else {fatalError()}
+        let user = users[indexPath.row]
+        
+        cell.textLabel?.text = user.name
 
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
