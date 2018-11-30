1fb7bb9dc5c83fefdd79c4abbd6394e733ca5f21
Sean Hendrix <hendoware@gmail.com>
2018-11-30 11:04:40 -0500

    add user controller file

---
+++ Random Users/UserController.swift
@@ -0,0 +1,15 @@
+//
+//  UserController.swift
+//  Random Users
+//
+//  Created by Sean Hendrix on 11/30/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+class UserController {
+
+
+}
+
