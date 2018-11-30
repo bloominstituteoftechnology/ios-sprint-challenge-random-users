99fb97ff76ea682a93d6bf29f6132976ee617035
Sean Hendrix <hendoware@gmail.com>
2018-11-30 11:02:39 -0500

    added tableviewcontroller, cell and detailviewcontroller

---
+++ Random Users/UserDetailsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  UserDetailsViewController.swift
+//  Random Users
+//
+//  Created by Sean Hendrix on 11/30/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailsViewController: UIViewController {
+
+
+}
