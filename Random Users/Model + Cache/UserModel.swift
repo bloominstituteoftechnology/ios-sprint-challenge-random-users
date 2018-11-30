3288d9b8da0ba0b5707ae5befa9ce1b16164d979
Jerrick Warren <Jerrick.Warren@gmail.com>
2018-11-30 08:44:56 -0800

    model completed

--- Random Users/Model + Cache/UserModel.swift
+++ Random Users/Model + Cache/UserModel.swift
@@ -9,14 +9,63 @@
 import Foundation
 import UIKit
 
-struct User: Codable {
+struct User: Decodable {
     
+    let name: String
+    let email: String
+    let phone: String
+    let thumbnailURL: URL
+    let fullSizeURL: URL
     
+    enum CodingKeys: String, CodingKey {
+        case name
+        case email
+        case phone
+        case picture
+        
+        enum NameCodingKeys: String, CodingKey {
+            case title
+            case first
+            case last
+        }
+        
+        enum PictureCodingKeys: String, CodingKey {
+            case large // for the DetailVC
+            case thumbnail // for the tableVC
+        }
+    }
     
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let email = try container.decode(String.self, forKey: .email)
+        let phone = try container.decode(String.self, forKey: .phone)
+        
+        let nameContainer = try container.nestedContainer(keyedBy: CodingKeys.NameCodingKeys.self, forKey: .name)
+        let title = try nameContainer.decode(String.self, forKey: .title)
+        let first = try nameContainer.decode(String.self, forKey: .first)
+        let last = try nameContainer.decode(String.self, forKey: .last)
+        
+        let fullName = "\(title.capitalized) + \(first.capitalized) + \(last.capitalized)"
+        
+        
+        let imageContainer = try container.nestedContainer(keyedBy: CodingKeys.PictureCodingKeys.self, forKey: .picture)
+        let thumbnailString = try imageContainer.decode(String.self, forKey: .thumbnail)
+        let thumbnailURL = URL(string: thumbnailString)!
+        
+        let fullSizeString = try imageContainer.decode(String.self, forKey: .large)
+        let fullSizeURL = URL(string: fullSizeString)!
+        
+        self.email = email
+        self.phone = phone
+        self.name = fullName
+        self.thumbnailURL = thumbnailURL
+        self.fullSizeURL = fullSizeURL
+    }
     
 }
 
 
-struct Results: Codable {
+struct Results: Decodable {
     let results: [User]
 }
