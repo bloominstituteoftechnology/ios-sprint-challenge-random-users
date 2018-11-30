1bf528e100e2cf7cb77e94b352d26cacdbc6ecb0
Sean Hendrix <hendoware@gmail.com>
2018-11-30 11:01:00 -0500

    first commit

---
+++ Random Users/UserTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  UserTableViewCell.swift
+//  Random Users
+//
+//  Created by Sean Hendrix on 11/30/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+
+}
---
+++ Random Users/UsersTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Random Users
+//
+//  Created by Sean Hendrix on 11/30/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+
+class UsersTableViewController: UITableViewController {
+    
+    
+    
+}
