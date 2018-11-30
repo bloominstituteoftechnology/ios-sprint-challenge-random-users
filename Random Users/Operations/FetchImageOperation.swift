2c932a26656d89ad0f0d211af5d6db8ac897c7f4
Sean Hendrix <hendoware@gmail.com>
2018-11-30 11:05:30 -0500

    add FetchImageOperation file

---
+++ Random Users/Operations/FetchImageOperation.swift
@@ -0,0 +1,48 @@
+//
+//  FetchImageOperation.swift
+//  Random Users
+//
+//  Created by Sean Hendrix on 11/30/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+class FetchImageOperation: ConcurrentOperation {
+    
+    // MARK:- Initializer
+    init(url: String) {
+        self.url = URL(string: url)!
+    }
+    
+    // MARK:- Properties & types
+    var url: URL
+    var imageData: Data?
+    
+    var dataTask: URLSessionDataTask {
+        return URLSession.shared.dataTask(with: url) { (data, _, error) in
+            
+            if let _ = error { return }
+            
+            guard let data = data else {
+                NSLog("No data found while performing the data task")
+                return
+            }
+            
+            self.imageData = data
+            
+            defer { self.state = .isFinished }
+        }
+    }
+    
+    // MARK:- NSOperation overrides
+    override func start() {
+        state = .isExecuting
+        dataTask.resume()
+    }
+    
+    override func cancel() {
+        dataTask.cancel()
+    }
+    
+}
