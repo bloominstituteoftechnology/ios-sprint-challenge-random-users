51960c9ab7eb545e0f93c23aa51d0ed34308f8ea
Ben Hakes <bhhakes@gmail.com>
2019-01-25 10:17:44 -0600

    set up basic UI and apappearance

---
+++ Random Users/View Controllers/ContactDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ContactDetailViewController.swift
+//  Random Users
+//
+//  Created by Benjamin Hakes on 1/25/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class ContactDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var contactImageView: UIImageView!
+    @IBOutlet weak var contactNameLabel: UILabel!
+    @IBOutlet weak var contactPhoneNumberLabel: UILabel!
+    @IBOutlet weak var contactEmailLabel: UILabel!
+
+}
---
+++ Random Users/View Controllers/RandomContactsTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  RandomContactsTableViewController.swift
+//  Random Users
+//
+//  Created by Benjamin Hakes on 1/25/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class RandomContactsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RandomContactCell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+}
