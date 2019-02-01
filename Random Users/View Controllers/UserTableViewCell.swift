a96621f015873df80be9285092c6f721dcbb6ac5
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2019-02-01 11:40:28 -0600

    Table View Cell set up complete

--- Random Users/View Controllers/UserTableViewCell.swift
+++ Random Users/View Controllers/UserTableViewCell.swift
@@ -9,7 +9,24 @@ class UserTableViewCell: UITableViewCell {
     
     @IBOutlet weak var userNameLabel: UILabel!
     
+    var user: User? {
+        didSet {
+            updateViews()
+        }
+    }
     
+    private func updateViews() {
+        
+        // Check to make sure there is a user
+        guard let user = user else { return }
+        
+        userNameLabel.text = "\(User.UserKeys.Name.title) \(User.UserKeys.Name.first) \(User.UserKeys.Name.last)"
+        
+        guard let url = URL(string: user.picture), let imageData = try? Data(contentsOf: url) else { return }
+        
+        userImage.image = UIImage(data: imageData)
+        
+    }
     
     
 }
--- Random Users/View Controllers/UsersTableViewController.swift
+++ Random Users/View Controllers/UsersTableViewController.swift
@@ -3,7 +3,20 @@ import UIKit
 
 class UsersTableViewController: UITableViewController {
     
-    
+    // Array of users
+    var users: [User] = [] {
+        
+        // Anytime this variable changes...
+        didSet {
+            
+            // Reload UIKit on the main queue
+            DispatchQueue.main.async {
+                // ...reload the table view
+                self.tableView.reloadData()
+            }
+        }
+        
+    }
     
     
 }
