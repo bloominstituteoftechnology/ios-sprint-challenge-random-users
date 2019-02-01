4160a79a722a08d393de953d444a48829b093455
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2019-02-01 10:24:14 -0600

    User interface set up

---
+++ Random Users/Resources/UserDetailViewController.swift
@@ -0,0 +1,15 @@
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+    
+    @IBOutlet weak var userImage: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var userPhoneLabel: UILabel!
+    
+    @IBOutlet weak var userEmailLabel: UILabel!
+    
+    
+}
---
+++ Random Users/Resources/UserTableViewCell.swift
@@ -0,0 +1,12 @@
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+    
+    static let reuseIdentifier = "usercell"
+    
+    @IBOutlet weak var userImage: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+}
