a5395abcf00956f0bb1a05eabcffacf62809810d
Moses Robinson <mosesjrobinson@icloud.com>
2019-02-22 10:16:13 -0800

    app conforms to custom decodable

--- Random Users/Model/User.swift
+++ Random Users/Model/User.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-struct User: Codable {
+struct User: Decodable {
     
     let name: String
     let email: String
@@ -37,13 +37,41 @@ struct User: Codable {
     
     // MARK: - DECODER
     
-    
-    
-    // MARK: - ENCODER
-    
-    
-    
-    init(name: String, email: String, phone: String, imageURL: URL, largeImageURL: URL) {
+    init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let email = try container.decode(String.self, forKey: .email)
+        let phone = try container.decode(String.self, forKey: .phone)
+        
+        let nameContainer = try container.nestedContainer(keyedBy: CodingKeys.NameCodingKeys.self, forKey: .name)
+        let title = try nameContainer.decode(String.self, forKey: .title)
+        let first = try nameContainer.decode(String.self, forKey: .first)
+        let last = try nameContainer.decode(String.self, forKey: .last)
+        
+        let name = "\(title) \(first) \(last)"
+        
+        let smallImageContainer = try container.nestedContainer(keyedBy: CodingKeys.PictureCodingKeys.self, forKey: .imageURL)
+        let imageURL = try smallImageContainer.decode(URL.self, forKey: .thumbnail)
+        
+        let largeImageContainer = try container.nestedContainer(keyedBy: CodingKeys.PictureCodingKeys.self, forKey: .largeImageURL)
+        let largeImageURL = try largeImageContainer.decode(URL.self, forKey: .large)
+        
         (self.name, self.email, self.phone, self.imageURL, self.largeImageURL) = (name, email, phone, imageURL, largeImageURL)
+    }
+}
+
+struct Users: Decodable {
+    let results: [User]
+    
+    enum CodingKeys: String, CodingKey {
+        case results
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let results = try container.decode([User].self, forKey: .results)
+        self.results = results
     }
 }
