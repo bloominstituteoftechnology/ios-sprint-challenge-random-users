ad578da789dcb056c4f72548cc67d5b7750950ea
Jocelyn Stuart <luluscomputer@jocelyns-macbook-air.local>
2019-02-22 11:23:20 -0500

    adds IB elements and corresponding swift files

---
+++ Random Users/Resources/UserDetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  UserDetailViewController.swift
+//  Random Users
+//
+//  Created by Jocelyn Stuart on 2/22/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    @IBOutlet weak var userFullSizeImage: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    
+
+}
---
+++ Random Users/Resources/UserTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  UserTableViewCell.swift
+//  Random Users
+//
+//  Created by Jocelyn Stuart on 2/22/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+    @IBOutlet weak var imageThumbnail: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    
+    
+    
+
+}
