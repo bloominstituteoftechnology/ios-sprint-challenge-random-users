8a824d920525f505a82f48ec4c8f21200c899337
Moses Robinson <mosesjrobinson@icloud.com>
2019-02-22 10:30:29 -0800

    application now fetching users data

--- Random Users/Model Controllers/ModelClient.swift
+++ Random Users/Model Controllers/ModelClient.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
-private let baseURL = URL(string: "https://randomuser.me/api/?format=json&inc=name,email,phone,picture&results=1000")!
+private let baseURL = URL(string: "https://randomuser.me/api/?format=json&inc=name,email,phone,picture")!
 
 class ModelClient {
     
-    
-    
-    
-    
-    
-    
+    func fetchUsers(resultsNumber: String, completion: @escaping (Error?) -> Void) {
+        
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let queryItem = URLQueryItem(name: "results", value: resultsNumber)
+        urlComponents?.queryItems = [queryItem]
+        
+        guard let requestURL = urlComponents?.url else {
+            NSLog("No URL found.")
+            completion(NSError())
+            return
+        }
+        
+        let dataTask = URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
+            if let error = error {
+                NSLog("could not find data: \(error)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("Could not find data. No data returned.")
+                completion(error)
+                return
+            }
+            
+            do {
+                let jsonDecoder = JSONDecoder()
+                
+                let decodedUsers = try jsonDecoder.decode(Users.self, from: data)
+                self.users = decodedUsers.results
+                completion(nil)
+            } catch {
+                NSLog("Unable to decode data into result: \(error)")
+                completion(error)
+                return
+            }
+        }
+        dataTask.resume()
+    }
     
     // MARK: - Properties
     
-    
+    private var users: [User] = []
 }
