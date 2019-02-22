7cb4f0df2a734d88a5c0723d27ea67441e09bec4
Frulwinn <frulwinn@gmail.com>
2019-02-22 08:44:43 -0800

    added outlets

--- Random Users/View Controllers/UserDetailViewController.swift
+++ Random Users/View Controllers/UserDetailViewController.swift
@@ -1,22 +1,16 @@
 import UIKit
 
 class UserDetailViewController: UIViewController {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var pictureView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Random Users/Views/UserTableViewCell.swift
+++ Random Users/Views/UserTableViewCell.swift
@@ -1,24 +1,9 @@
-//
-//  UserTableViewCell.swift
-//  Random Users
-//
-//  Created by Lambda_School_Loaner_34 on 2/22/19.
-//  Copyright © 2019 Erica Sadun. All rights reserved.
-//
-
 import UIKit
 
 class UserTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    //MARK: - Outlets
+    @IBOutlet weak var pictureView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
 }
