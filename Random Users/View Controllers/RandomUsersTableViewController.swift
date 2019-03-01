97a60c6700cc2f8bda0b68f5183dc145392dccaa
Paul Yi <yipaulc@gmail.com>
2019-03-01 10:34:54 -0800

    Implement cancellation

--- Random Users/View Controllers/RandomUsersTableViewController.swift
+++ Random Users/View Controllers/RandomUsersTableViewController.swift
@@ -42,6 +42,13 @@ class RandomUsersTableViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard let randomUser = randomUsers?[indexPath.row],
+            let phoneNumber = randomUser.phoneNumber else { return }
+        
+        activeOperations[phoneNumber]?[.thumbnail]?.cancel()
+    }
+    
     private func loadImage(forCell cell: RandomUserTableViewCell, forItemAt indexPath: IndexPath) {
         
         guard let randomUser = randomUsers?[indexPath.row],
@@ -70,44 +77,7 @@ class RandomUsersTableViewController: UITableViewController {
             activeOperations[phoneNumber] = [.thumbnail: thumbnailOperation]
         }
     }
-    
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -115,7 +85,6 @@ class RandomUsersTableViewController: UITableViewController {
             let detailVC = segue.destination as! RandomUserDetailViewController
         }
     }
-    
     
     // MARK: - Properties
     
