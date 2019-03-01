8c1c0b76fe625502c2d7e452d4c3dc6e2a306129
nateyoungren <nathanaelyoungren@gmail.com>
2019-03-01 09:44:27 -0800

    Setup Users tvc.

--- Random Users/View Controllers/UsersTableViewController.swift
+++ Random Users/View Controllers/UsersTableViewController.swift
@@ -12,16 +12,29 @@ class UsersTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        networkController.fetchUsers { (error) in
+            if let error = error {
+                NSLog("Erroring fetching users: \(error)")
+                return
+            }
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return networkController.users.count
     }
 
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath)
-
+        let user = networkController.users[indexPath.row]
+        
+        cell.textLabel?.text = "\(user.title.capitalized) \(user.first.capitalized) \(user.last.capitalized)"
+    
+        loadImage(for: cell, at: indexPath)
         return cell
     }
 
@@ -29,5 +42,27 @@ class UsersTableViewController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
     }
-
+    
+    func loadImage(for cell: UITableViewCell, at indexPath: IndexPath) {
+        
+        let user = networkController.users[indexPath.row]
+        
+        let fetchThumbnailImageOperation = FetchThumbnailImageOperation(user: user)
+        
+        let addImageOperation = BlockOperation {
+            
+            if let image = fetchThumbnailImageOperation.imageData {
+                cell.imageView?.image = UIImage(data: image)
+            }
+        }
+        
+        addImageOperation.addDependency(fetchThumbnailImageOperation)
+        
+        imageFetchQueue.addOperation(fetchThumbnailImageOperation)
+        OperationQueue.main.addOperation(addImageOperation)
+    }
+    
+    let networkController = NetworkController()
+    
+    let imageFetchQueue = OperationQueue()
 }
