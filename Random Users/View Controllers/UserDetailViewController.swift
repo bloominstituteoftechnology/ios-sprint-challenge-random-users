2a489400061c2d70d2463aebeea8e9d58d26b6e3
Frulwinn <frulwinn@gmail.com>
2019-03-01 08:19:29 -0800

    added objects to IB, hooked up outlets, and classed view controllers

---
+++ Random Users/View Controllers/UserDetailViewController.swift
@@ -0,0 +1,21 @@
+//Frulwinn
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+    
+    //MARK: - Properties
+    
+    //MARK: - Outlets
+    @IBOutlet weak var largeImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ Random Users/Views/UserTableViewCell.swift
@@ -0,0 +1,13 @@
+//Frulwinn
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    //MARK: - Properties
+    
+    //MARK: - Outlets
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
