5d691bb100f8784f00c87571d11b8f6b4af62719
Paul Yi <yipaulc@gmail.com>
2019-03-01 10:00:01 -0800

    Implement FetchLargeImageOperation

--- Random Users/Operations/FetchLargeImageOperation.swift
+++ Random Users/Operations/FetchLargeImageOperation.swift
@@ -6,6 +6,38 @@
 //  Copyright © 2019 Erica Sadun. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-
+class FetchLargeImageOperation: ConcurrentOperation {
+    
+    init(randomUser: RandomUser) {
+        self.randomUser = randomUser
+    }
+    
+    override func start() {
+        state = .isExecuting
+        
+        guard let url = randomUser.largeImageURL else { return }
+        dataTask = URLSession.shared.dataTask(with: url) { (data, _, error) in
+            defer { self.state = .isFinished}
+            
+            if let error = error {
+                NSLog("Error retrieving image: \(error)")
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data found.")
+                return
+            }
+            self.largeImage = UIImage(data: data)
+        }
+        dataTask?.cancel()
+    }
+    
+    // MARK: - Properties
+    
+    var randomUser: RandomUser
+    var largeImage: UIImage?
+    private var dataTask: URLSessionDataTask?
+}
