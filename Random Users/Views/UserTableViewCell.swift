379b8961ed02429bb1e7983134293256d74afadd
Frulwinn <frulwinn@gmail.com>
2019-03-01 09:33:31 -0800

    added prepare for reuse and update views to table view cell

--- Random Users/Views/UserTableViewCell.swift
+++ Random Users/Views/UserTableViewCell.swift
@@ -5,9 +5,28 @@ import UIKit
 class UserTableViewCell: UITableViewCell {
 
     //MARK: - Properties
+    static let reuseIdentifier = "userCell"
+    var user: User? {
+        didSet {
+            updateViews()
+        }
+    }
+    var indexPath: IndexPath?
     
     //MARK: - Outlets
     @IBOutlet weak var userImage: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     
+    
+    func updateViews() {
+        if let user = user {
+        nameLabel.text = "\(user.title) \(user.firstName) \(user.lastName)"
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        userImage.image = nil
+        indexPath = nil
+    }
 }
