2a1b2a63cf2628baa1629d1ff6611d120f2c20b8
Paul Yi <yipaulc@gmail.com>
2019-03-01 08:46:16 -0800

    Implement init(from: Decoder)

---
+++ Random Users/Model/RandomUserExtension.swift
@@ -0,0 +1,57 @@
+//
+//  RandomUserExtension.swift
+//  Random Users
+//
+//  Created by Paul Yi on 3/1/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+extension RandomUser: Decodable {
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case phoneNumber = "phone"
+        case emailAddress = "email"
+        case imageURLS = "pictures"
+        
+        enum NameCodingKeys: String, CodingKey {
+            case title
+            case first
+            case last
+        }
+        
+        enum ImageURLCodingKeys: String, CodingKey {
+            case thumbnail
+            case large
+        }
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let nameContainer = try container.nestedContainer(keyedBy: CodingKeys.NameCodingKeys.self, forKey: .name)
+        let title = try nameContainer.decode(String.self, forKey: .title)
+        let first = try nameContainer.decode(String.self, forKey: .first)
+        let last = try nameContainer.decode(String.self, forKey: .last)
+        
+        let name = "\(title) \(first) \(last)"
+        
+        let phoneNumber = try container.decode(String.self, forKey: .phoneNumber)
+        let emailAddress = try container.decode(String.self, forKey: .emailAddress)
+        
+        let imageContainer = try container.nestedContainer(keyedBy: CodingKeys.ImageURLCodingKeys.self, forKey: .imageURLS)
+        let thumbnailString = try imageContainer.decode(String.self, forKey: .thumbnail)
+        let largeString = try imageContainer.decode(String.self, forKey: .large)
+        
+        let thumbnailImageURL = URL(string: thumbnailString)
+        let largeImageURL = URL(string: largeString)
+        
+        self.name = name
+        self.phoneNumber = phoneNumber
+        self.emailAddress = emailAddress
+        self.thumbnailImageURL = thumbnailImageURL
+        self.largeImageURL = largeImageURL
+    }
+}
