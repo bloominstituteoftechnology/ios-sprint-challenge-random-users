c06d901d73aa90210ada14c344d2af57716d0ba1
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-07 08:43:10 -0700

    models

---
+++ Random Users/Models/Person.swift
@@ -0,0 +1,25 @@
+//
+//  Person.swift
+//  Random Users
+//
+//  Created by Hector Steven on 6/7/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+
+struct Person: Codable, Equatable {
+	
+	let name: String
+	let picture: String
+
+	
+	
+	
+	
+	
+}
