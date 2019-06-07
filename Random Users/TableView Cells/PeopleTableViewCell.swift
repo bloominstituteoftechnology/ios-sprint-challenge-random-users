e426deeab8a1056c9f393380601a614d7955ed2a
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-07 08:20:54 -0700

    TableView init

---
+++ Random Users/TableView Cells/PeopleTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PeopleTableViewCell.swift
+//  Random Users
+//
+//  Created by Hector Steven on 6/7/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class PeopleTableViewCell: UITableViewCell {
+
+
+
+}
