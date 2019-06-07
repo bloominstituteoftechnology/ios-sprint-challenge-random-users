0d7cbcc998305d1af141cf189af379c82e436726
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-07 11:53:39 -0700

    Cache<Key: Hashable, Value>

--- Random Users/Models/Cache.swift
+++ Random Users/Models/Cache.swift
@@ -10,5 +10,23 @@ import Foundation
 
 
 struct Cache<Key: Hashable, Value> {
+	private (set) var cache: [Key: Value] = [:]
+	let q = DispatchQueue(label: "cache")
+	
+	mutating func cache(value: Value, for key: Key) {
+		q.sync {
+			if let _ = cache[key] { return }
+			cache[key] = value
+		}
+	}
+	
+	func value(for key: Key) -> Value? {
+		var value: Value?
+		q.sync {
+			value = cache[key]
+		}
+		
+		return value
+	}
 	
 }
