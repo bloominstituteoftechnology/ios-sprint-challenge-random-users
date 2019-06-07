8404834a803b95b8f3d8f07b18033beaf6b6a4c4
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-07 08:57:16 -0700

    init(from decoder

--- Random Users/Models/Person.swift
+++ Random Users/Models/Person.swift
@@ -18,12 +18,13 @@ struct Person: Codable, Equatable {
 	let picture: String
 
 	
-	enum CodingKeys: String, CodingKey {
+	enum PerosnCodingKeys: String, CodingKey {
 		case name
 		case picture
 		
 		enum NameCodingKey: String, CodingKey {
-			case name
+			case first
+			case last
 		}
 		
 		enum PictureCodingKey: String, CodingKey {
@@ -33,6 +34,17 @@ struct Person: Codable, Equatable {
 		
 	}
 	
+	init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: PerosnCodingKeys.self)
+		
+		let nameContainer = try container.nestedContainer(keyedBy: PerosnCodingKeys.NameCodingKey.self, forKey: .name)
+		let first  = try nameContainer.decode(String.self, forKey: .first)
+		let last = try nameContainer.decode(String.self, forKey: .last)
+		name = first + " " + last
+		
+		let pictureContainer = try container.nestedContainer(keyedBy: PerosnCodingKeys.PictureCodingKey.self, forKey: .picture)
+		picture = try pictureContainer.decode(String.self, forKey: .thumbnail)
+	}
 	
 	
 }
