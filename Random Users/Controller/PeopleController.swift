e974f27425082f9693567925ecd7bbaf3e0356d6
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-07 08:39:11 -0700

    PeopleController

---
+++ Random Users/Controller/PeopleController.swift
@@ -0,0 +1,19 @@
+//
+//  PeopleController.swift
+//  Random Users
+//
+//  Created by Hector Steven on 6/7/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class PeopleController {
+	
+	
+	
+	
+	
+	
+	
+}
