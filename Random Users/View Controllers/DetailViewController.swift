7d5861e7eafbe15cc16a3262553270f198e82606
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-07 11:28:01 -0700

    detail setupView()

--- Random Users/View Controllers/DetailViewController.swift
+++ Random Users/View Controllers/DetailViewController.swift
@@ -14,9 +14,52 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
 
 	}
+	
+	func setupView() {
+		guard let person = person else { return }
+		
+		
+		nameLabel.text = person.name
+		phoneNumberLabel.text = person.phone
+		emailLabel.text = person.email
+		guard let url = URL(string: person.picture[0]) else {return}
+		
+		let shared = URLSession.shared
+		shared.dataTask(with: url) { data, response, error in
+			if let response = response as? HTTPURLResponse {
+				NSLog("Response Code: \(response.statusCode)")
+			}
+			
+			if let error = error {
+				NSLog("Error fetching data \(error)")
+				
+				return
+			}
+			
+			guard let data = data else { return }
+			print(data)
+			
+			let img = UIImage(data: data)
+			
+			DispatchQueue.main.async {
+				
+				self.personImageView.image = img
+			}
+			
+			
+			}.resume()
+		
+		
+		
+		
+		
+	}
+	
 
 	@IBOutlet var personImageView: UIImageView!
 	@IBOutlet var nameLabel: UILabel!
 	@IBOutlet var phoneNumberLabel: UILabel!
 	@IBOutlet var emailLabel: UILabel!
+	
+	var person: Person?
 }
