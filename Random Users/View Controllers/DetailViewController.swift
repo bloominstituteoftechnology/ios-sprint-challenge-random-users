b4bf6dd8f922ab6ddb66c875a5651cf1d9d5aeb4
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-07 11:24:33 -0700

    DetailViewController

---
+++ Random Users/View Controllers/DetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailViewController.swift
+//  Random Users
+//
+//  Created by Hector Steven on 6/7/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+	}
+
+	@IBOutlet var personImageView: UIImageView!
+	@IBOutlet var nameLabel: UILabel!
+	@IBOutlet var phoneNumberLabel: UILabel!
+	@IBOutlet var emailLabel: UILabel!
+}
