201e80bd4ac6a226552df9cf7f77e324511fd1c2
victor <ruizdvictor@gmail.com>
2019-06-07 16:47:01 -0400

    added table view cell properties

---
+++ Random Users/Views/UserTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  UserTableViewCell.swift
+//  Random Users
+//
+//  Created by Victor  on 6/7/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var nameLabel: UILabel
+    @IBOutlet weak var imageView: UIImageView
+    
+    
+    
+}
