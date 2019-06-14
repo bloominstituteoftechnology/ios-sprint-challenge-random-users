ea38cd32a5c870caeb1520bea2278ec8e505d832
Diante Lewis-Jolley <diantelewis-jolley@Diantes-MBP.frontier.com>
2019-06-14 14:18:52 -0400

    forgot segue

--- Random Users/UserTableViewController.swift
+++ Random Users/UserTableViewController.swift
@@ -112,14 +112,21 @@ class UserTableViewController: UITableViewController {
 
     }
 
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+
+        if segue.identifier == "DetailSegue" {
+            guard let DetailVC = segue.destination as? DetailViewController,
+                let indexPath = tableView.indexPathForSelectedRow else { return }
+            DetailVC.user = users?[indexPath.row]
+        }
     }
-    */
+
+
 
 }
