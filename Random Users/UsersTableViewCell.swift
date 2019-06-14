adb6c20c5f93a11dde7418b672a27366881bf01d
Michael R.F <mrflowers22@uwalumni.com>
2019-06-14 10:23:20 -0700

    connected the iboutlets to tableviewcell code file

--- Random Users/UsersTableViewCell.swift
+++ Random Users/UsersTableViewCell.swift
@@ -10,15 +10,7 @@ import UIKit
 
 class UsersTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
 }
