41b0aaf5e36fad10a7c29c8351e60ededfa7066f
Michael R.F <mrflowers22@uwalumni.com>
2019-06-14 10:20:20 -0700

    added applicable code files to project navigator

---
+++ Random Users/DetailUserViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailUserViewController.swift
+//  Random Users
+//
+//  Created by Michael Flowers on 6/14/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class DetailUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
