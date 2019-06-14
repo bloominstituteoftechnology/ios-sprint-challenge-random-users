869c24b44dd4dd3d8037c85fa66adb2a8d8c02af
Sameera Leola <pixelandbyte@gmail.com>
2019-06-14 17:15:46 -0500

    Working on large image in detail

--- Random Users/View Controllers/UserDetailViewController.swift
+++ Random Users/View Controllers/UserDetailViewController.swift
@@ -10,25 +10,52 @@ import UIKit
 
 class UserDetailViewController: UIViewController {
 
+    // MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         // Make sure we have a user
         guard let user = user else { return }
         
-        // Display the data
-        userNameLabel.text = user.name
-        phoneNumberLabel.text = user.phoneNumber
-        emailLabel.text = user.email
+        // Get and display the large image
+       // displayUserImage(user.largeImageURL!)
+        let fetchLargeImage = LargeImageFetch(randomUser: user)
+        let blockOperation = BlockOperation {
+            guard let largeImage = fetchLargeImage.largeImage else { return }
+            self.userImageView.image = largeImage
+        }
+        blockOperation.addDependency(fetchLargeImage)
+        fetchRandomUserQueue.addOperation(blockOperation)
+        OperationQueue.main.addOperation(blockOperation)
         
-        
+        // Display the text data
+         populateLabels(user)
     }
     
     
+    // MARK: - Display user details
+    func displayUserImage(_ imageURL: URL) {
+        let fetchLargeImage = LargeImageFetch(randomUser: user!)
+        let blockOperation = BlockOperation {
+            guard let largeImage = fetchLargeImage.largeImage else { return }
+            self.userImageView.image = largeImage
+        }
+        blockOperation.addDependency(fetchLargeImage)
+        fetchRandomUserQueue.addOperation(blockOperation)
+        OperationQueue.main.addOperation(blockOperation)
+    }
     
-
+    // Display the user name, phone, and email
+    func populateLabels(_ user: RandomUser) {
+        userNameLabel.text = user.name
+        phoneNumberLabel.text = user.phoneNumber
+        emailLabel.text = user.email
+    }
+    
+    
     // MARK: - Properties
     var user: RandomUser?
     let fetchRandomUserQueue = OperationQueue()
+    
     
     // MARK: - Outlets
     @IBOutlet weak var userImageView: UIImageView!
