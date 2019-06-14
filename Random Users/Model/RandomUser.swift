be3afef2e63375aae7f6201c93d87d5effe92537
Sameera Leola <pixelandbyte@gmail.com>
2019-06-14 11:49:07 -0500

    Created struct to hold 1,000 random users

--- Random Users/Model/RandomUser.swift
+++ Random Users/Model/RandomUser.swift
@@ -20,12 +20,11 @@ struct RandomUser: Equatable, Decodable {
         case large
     }
     
-    enum UserKeys: String, CodingKey {
+    enum CodingKeys: String, CodingKey {
         case name
         case phoneNumber = "phone"
         case email
         case imagesURL = "picture"
-    }
     
     enum UserNameKeys: String, CodingKey {
         case title
@@ -35,6 +34,49 @@ struct RandomUser: Equatable, Decodable {
     
     enum ImageURLKeys: String, CodingKey {
         case thumbnail
-        case large
+        case largeImage
     }
+}
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        // Decode the user name
+        let nameContainer = try container.nestedContainer(keyedBy: CodingKeys.UserNameKeys.self, forKey: .name)
+        let title = try nameContainer.decode(String.self, forKey: .title)
+        let first = try nameContainer.decode(String.self, forKey: .first)
+        let last = try nameContainer.decode(String.self, forKey: .last)
+        let name = "\(title) \(first) \(last)"
+        
+        // Decode phone number
+        let phoneNumber = try container.decode(String.self, forKey: .phoneNumber)
+        
+        // Decode email address
+        let email = try container.decode(String.self, forKey: .email)
+        
+        // Decode images URL strings
+        let imagesContainer = try container.nestedContainer(keyedBy: CodingKeys.ImageURLKeys.self, forKey: .imagesURL)
+        let thumbnailString = try imagesContainer.decode(String.self, forKey: .thumbnail)
+        let largeImageString = try imagesContainer.decode(String.self, forKey: .largeImage)
+        
+        // Now create image URLs
+        let thumbnailURL = URL(string: thumbnailString)
+        let largeImageURL = URL(string: largeImageString)
+        
+        // Create the user data fields
+        self.name = name
+        self.phoneNumber = phoneNumber
+        self.email = email
+        self.thumbNailURL = thumbnailURL
+        self.largeImageURL = largeImageURL
+    }
+}
+
+// 1000 random users
+struct RandomUsers: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case randomUsers = "results"
+    }
+    
+    var randomUsers: [RandomUser]
 }
