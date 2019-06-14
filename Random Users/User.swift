174263628f96829102a4109651112cd7e2c8ab49
Michael R.F <mrflowers22@uwalumni.com>
2019-06-14 10:49:24 -0700

    made User struct

---
+++ Random Users/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Random Users
+//
+//  Created by Michael Flowers on 6/14/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    
+}
