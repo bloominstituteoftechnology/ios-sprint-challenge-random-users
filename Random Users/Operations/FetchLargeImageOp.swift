e87f9987bc0a621f2bc60776c0c3094a05bf8ebb
Lisa Sampson <lisasampson0918@gmail.com>
2019-06-14 15:58:14 -0400

    Set up image operations

--- Random Users/Operations/FetchLargeImageOp.swift
+++ Random Users/Operations/FetchLargeImageOp.swift
@@ -6,4 +6,49 @@
 //  Copyright © 2019 Erica Sadun. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class FetchLargeImageOp: ConcurrentOperation {
+    
+    // MARK: - Properties
+    var randomUser: RandomUser
+    var largeImage: UIImage?
+    private var dataTask: URLSessionDataTask?
+    
+    // MARK: - Init
+    init(randomUser: RandomUser) {
+        self.randomUser = randomUser
+    }
+    
+    // MARK: - Start and Cancel
+    override func start() {
+        state = .isExecuting
+        
+        let url = randomUser.largeImageURL
+        
+        dataTask = URLSession.shared.dataTask(with: url) { (data, _, error) in
+            defer { self.state = .isFinished }
+            
+            if let error = error {
+                NSLog("Error retrieving image: \(error)")
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("Error finding data.")
+                return
+            }
+            
+            guard let largeImage = UIImage(data: data) else {
+                NSLog("Error trying to construct image from url.")
+                return
+            }
+            self.largeImage = largeImage
+        }
+        dataTask?.resume()
+    }
+    
+    override func cancel() {
+        dataTask?.cancel()
+    }
+}
--- Random Users/Operations/FetchThumbnailOp.swift
+++ Random Users/Operations/FetchThumbnailOp.swift
@@ -6,4 +6,49 @@
 //  Copyright © 2019 Erica Sadun. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class FetchThumbnailOp: ConcurrentOperation {
+    
+    // MARK: - Properties
+    var randomUser: RandomUser
+    var thumbnailImage: UIImage?
+    private var dataTask: URLSessionDataTask?
+    
+    // MARK: - Init
+    init(randomUser: RandomUser) {
+        self.randomUser = randomUser
+    }
+    
+    // MARK: - Start and Cancel
+    override func start() {
+        state = .isExecuting
+        
+        let url = randomUser.thumbnailImageURL
+        
+        dataTask = URLSession.shared.dataTask(with: url) { (data, _, error) in
+            defer { self.state = .isFinished }
+            
+            if let error = error {
+                NSLog("Error retrieving image: \(error)")
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("Error finding data.")
+                return
+            }
+            
+            guard let thumbnailImage = UIImage(data: data) else {
+                NSLog("Error trying to construct image from url.")
+                return
+            }
+            self.thumbnailImage = thumbnailImage
+        }
+        dataTask?.resume()
+    }
+    
+    override func cancel() {
+        dataTask?.cancel()
+    }
+}
