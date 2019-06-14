43fb01fd2e67be7e95e558a1c5ec9c190192871a
Ryan Murphy <thomasryanmurphy@gmail.com>
2019-06-14 11:32:12 -0400

    Storyboard and Model set up

---
+++ Random Users/Resources/UsersDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  UsersDetailViewController.swift
+//  Random Users
+//
+//  Created by Ryan Murphy on 6/14/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UsersDetailViewController: UIViewController {
+
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bigImageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
