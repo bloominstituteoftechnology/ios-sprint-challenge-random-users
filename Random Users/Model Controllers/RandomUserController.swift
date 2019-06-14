153672bd0a02b81f32bc94e9593ee13822f78072
Lisa Sampson <lisasampson0918@gmail.com>
2019-06-14 15:41:47 -0400

    Set up model controller

--- Random Users/Model Controllers/RandomUserController.swift
+++ Random Users/Model Controllers/RandomUserController.swift
@@ -7,3 +7,52 @@
 //
 
 import Foundation
+
+// MARK: - BaseURL
+private let baseURL = URL(string: "https://randomuser.me/api/?format=json&inc=name,email,phone,picture")!
+
+class RandomUserController {
+    
+    // MARK: - Properties
+    var randomUsers: [RandomUser] = []
+    
+    // MARK: - Fetch Method
+    func fetchRandomUsers(results: String, completion: @escaping (Error?) -> Void = { _ in }) {
+        
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let queryItem = URLQueryItem(name: "results", value: results)
+        urlComponents?.queryItems = [queryItem]
+        
+        guard let requestURL = urlComponents?.url else {
+            NSLog("Error creating request url.")
+            completion(NSError())
+            return
+        }
+        
+        URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
+            if let error = error {
+                NSLog("Error starting data task: \(error)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("Error finding data.")
+                completion(error)
+                return
+            }
+            
+            do {
+                let jsonDecoder = JSONDecoder()
+                
+                let decodedUsers = try jsonDecoder.decode(RandomUsers.self, from: data)
+                self.randomUsers = decodedUsers.results
+                completion(nil)
+            } catch {
+                NSLog("Error trying to decode random users: \(error)")
+                completion(error)
+                return
+            }
+        }.resume()
+    }
+}
