bf962d3def7b10ac738834c2f903eb9ddea9cd75
Lisa Sampson <lisasampson0918@gmail.com>
2019-06-14 16:30:58 -0400

    Set up basic detail view functionality

--- Random Users/View Controllers/RandomUserDetailViewController.swift
+++ Random Users/View Controllers/RandomUserDetailViewController.swift
@@ -10,14 +10,44 @@ import UIKit
 
 class RandomUserDetailViewController: UIViewController {
     
-    // MARK: - Properties
+    // MARK: - Properties and Outlets
     var randomUserController: RandomUserController?
-    var randomUser: RandomUser?
+    var randomUser: RandomUser? {
+        didSet {
+            updateViews()
+        }
+    }
+    private let randomUserFetchQueue = OperationQueue()
+    private var activeOps: [String: FetchLargeImageOp] = [:]
+    private var largeImageCache: Cache<String, UIImage> = Cache()
 
+    @IBOutlet weak var randomImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    // MARK: - View Loading Methods
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        updateViews()
     }
     
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        guard let randomUser = randomUser else { return }
+        activeOps[randomUser.phone]?.cancel()
+    }
+    
+    private func updateViews() {
+        guard let randomUser = randomUser else { return }
+        
+        nameLabel.text = randomUser.name
+        phoneLabel.text = randomUser.phone
+        emailLabel.text = randomUser.email
+    }
+    
+    private func loadImage(for randomUser: RandomUser) {
+        
+    }
 }
