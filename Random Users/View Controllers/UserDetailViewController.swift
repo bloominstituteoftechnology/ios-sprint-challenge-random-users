d2eba804f05b04f0e70a5dc397d88770e5d58948
Mitchell Budge <mitchellbudge@gmail.com>
2019-06-14 09:17:28 -0600

    Configured app skeleton

---
+++ Random Users/View Controllers/UserDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UserDetailViewController.swift
+//  Random Users
+//
+//  Created by Mitchell Budge on 6/14/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
