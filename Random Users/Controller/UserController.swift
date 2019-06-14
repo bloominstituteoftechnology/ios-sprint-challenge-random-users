6c8dcac7d04ecb11354b26839d5f36edb54d325c
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-14 08:41:25 -0700

    User, UserController

---
+++ Random Users/Controller/UserController.swift
@@ -0,0 +1,20 @@
+//
+//  UserController.swift
+//  Random Users
+//
+//  Created by Hector Steven on 6/14/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+
+class UserController {
+	
+	
+	
+	
+	
+	
+	private (set) var users: [User] = []
+}
---
+++ Random Users/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Random Users
+//
+//  Created by Hector Steven on 6/14/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+struct User: Decodable {
+	
+}
