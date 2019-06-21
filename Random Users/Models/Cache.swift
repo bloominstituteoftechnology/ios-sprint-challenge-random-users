6a4d5fcafb1b7b37e70158290eee634b767510c6
Michael R.F <mrflowers22@uwalumni.com>
2019-06-21 13:21:23 -0700

    create class for cache and added a serial queue to make them thread safe

---
+++ Random Users/Models/Cache.swift
@@ -0,0 +1,31 @@
+//
+//  Cache.swift
+//  Random Users
+//
+//  Created by Michael Flowers on 6/21/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+class Cache<Key: Hashable, Value> {
+    private var cachedItems = [ Key : Value ]()
+    
+    //create a private queue
+    private let queue = DispatchQueue(label: "com.michaelFlowers.lambdaSchoolSprint.Randomusers")
+    
+    //add items to the cache
+    func cacheAdd(value: Value, forKey key: Key){
+        //add to cache on the queue we just created, background serial queue
+        queue.async {
+            self.cachedItems[key] = value
+        }
+    }
+    
+    //return item/pop from cache
+    func returnCachedValue(forKey key: Key) -> Value? {
+        return queue.sync {
+            cachedItems[key]
+        }
+    }
+}
