b140839282ee8ba9a06f98efe2d1312ca9a40775
Michael R.F <mrflowers22@uwalumni.com>
2019-06-21 08:40:03 -0700

    created tableView, tableViewCell and viewController source code files

---
+++ Random Users/DetailUserViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailUserViewController.swift
+//  Random Users
+//
+//  Created by Michael Flowers on 6/21/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class DetailUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Random Users/Resources/RandomUserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RandomUserTableViewCell.swift
+//  Random Users
+//
+//  Created by Michael Flowers on 6/21/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class RandomUserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
