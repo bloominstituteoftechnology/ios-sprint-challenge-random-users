d178eab6d2e7014e8b6fb97ad46693b1d20d8edf
John Pitts <jgpitts@gmail.com>
2019-06-21 11:29:17 -0400

    Added 3 cocoa touch files for TableView, CustomCell, and DetailVC, connected them to storyboard w reuse id for table cell, cleaned up TableView file, added outlets from labels and imageViews to proper files

---
+++ Random Users/Resources/VIEW CONTROLLERS/UsersTableViewController.swift
@@ -0,0 +1,58 @@
+//
+//  UsersTableViewController.swift
+//  Random Users
+//
+//  Created by John Pitts on 6/21/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UsersTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+        
+        // cell
+
+        return cell
+    }
+
+
+
+
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        
+        // Pass User object to custom cell
+    }
+
+}
