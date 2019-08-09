227fc92d99c3931dcde8c88cd9a58553df61baea
Kat Milton <mltnkat@gmail.com>
2019-08-09 08:22:33 -0700

    add initial commit.

---
+++ Random Users/View Controllers/UserDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  UserDetailViewController.swift
+//  Random Users
+//
+//  Created by Kat Milton on 8/9/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+    
+    // MARK: - Properties
+    @IBOutlet var userImageView: UIImageView!
+    @IBOutlet var userNameLabel: UILabel!
+    @IBOutlet var userNumberLabel: UILabel!
+    @IBOutlet var userEmailLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Random Users/View/UserTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  UserTableViewCell.swift
+//  Random Users
+//
+//  Created by Kat Milton on 8/9/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+    
+    // MARK: - Properties
+    @IBOutlet var userImageView: UIImageView!
+    @IBOutlet var userNameLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
