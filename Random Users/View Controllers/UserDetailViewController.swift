1788c3ed4bf0207e95eccb6c931717d0071c49b8
Stephanie Bowles <sbowl001@fiu.edu>
2019-08-16 00:30:46 +0800

    detailcontroller

--- Random Users/View Controllers/UserDetailViewController.swift
+++ Random Users/View Controllers/UserDetailViewController.swift
@@ -16,21 +16,37 @@ class UserDetailViewController: UIViewController {
     @IBOutlet weak var phoneLabel: UILabel!
     
     @IBOutlet weak var emailLabel: UILabel!
+    
+    
+    var user: User? {
+        didSet {
+            self.updateViews()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.updateViews()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func updateViews() {
+        guard let user = self.user,
+        let name = nameLabel,
+        let email = emailLabel,
+        let phone = phoneLabel,
+        let image = largeImageView else {return}
+        
+        name.text = user.name
+        phone.text = user.phone
+        email.text  = user.email
+        
+        let url = user.largePic
+        if let data = try? Data(contentsOf: url) {
+           image.image = UIImage(data: data)
+        }
+        
     }
-    */
+  
+    
 
 }
