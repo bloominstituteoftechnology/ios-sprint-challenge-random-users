d1e1398ef140d003b0a475234eb7ec83ed13ad2e
Stephanie Bowles <sbowl001@fiu.edu>
2019-08-16 00:20:02 +0800

    fetchpic

--- Random Users/Operations/FetchImageOperation.swift
+++ Random Users/Operations/FetchImageOperation.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+class FetchImageOperation: ConcurrentOperation {
+    private (set) var imageData: Data?
+    private var dataTask: URLSessionDataTask?
+    
+    let user: User
+    
+    init(user: User) {
+        self.user = user
+        super.init()
+    }
+    
+    override func start(){
+        super.start()
+        state = .isExecuting
+        
+        let url = self.user.thumbnailPic
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            defer { self.state = .isFinished}
+            if self.isCancelled {return}
+            
+            if let error = error {
+                NSLog("Error fetching image data: \(error)")
+                return
+            }
+            self.imageData = data
+            
+        }
+        task.resume()
+        self.dataTask = task
+    }
+    override func cancel() {
+        super.cancel()
+        if let task = self.dataTask {
+            task.cancel()
+        }
+    }
+}
