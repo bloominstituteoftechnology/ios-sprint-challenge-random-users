5ac998831ccf945ba939677270fbc8a87a8d5203
Stephanie Bowles <sbowl001@fiu.edu>
2019-08-15 23:27:53 +0800

    cache and model

---
+++ Random Users/Controllers/NetworkController.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkController.swift
+//  Random Users
+//
+//  Created by Stephanie Bowles on 8/15/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import Foundation
---
+++ Random Users/Operations/FetchImageOperation.swift
@@ -0,0 +1,9 @@
+//
+//  FetchImageOperation.swift
+//  Random Users
+//
+//  Created by Stephanie Bowles on 8/15/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import Foundation
