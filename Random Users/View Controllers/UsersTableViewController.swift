209a14db489c916b9bdd4139f133f097cdb06f88
Stephanie Bowles <sbowl001@fiu.edu>
2019-08-16 00:59:32 +0800

    seems to work but some sort of error on Random users

--- Random Users/View Controllers/UsersTableViewController.swift
+++ Random Users/View Controllers/UsersTableViewController.swift
@@ -10,81 +10,99 @@ import UIKit
 
 class UsersTableViewController: UITableViewController {
 
+    
+    let networkController = NetworkController()
+    let cache = Cache<String, Data>()
+    let picFetchQueue = OperationQueue()
+    var operations = [String: Operation]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.networkController.fetchUsers { (error) in
+            if let error = error {
+                NSLog("error fetching users: \(error)")
+                return
+            }
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            
+        }
+       
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+ 
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.networkController.users.count
     }
 
-    /*
+ 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath)
 
-        // Configure the cell...
+        self.loadPic(forCell: cell, forItemAt: indexPath)
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    private func loadPic(forCell cell: UITableViewCell, forItemAt indexPath: IndexPath) {
+        let userReference = self.networkController.users[indexPath.item]
+        
+        if let cachedData = self.cache.value(key: userReference.name) {
+            cell.imageView?.image = UIImage(data: cachedData)
+            cell.textLabel?.text = userReference.name
+            return
+                
+            }
+        
+        let fetchPictureOperation = FetchImageOperation(user: userReference)
+        
+        let cachedOperation = BlockOperation {
+            if let data = fetchPictureOperation.imageData {
+                self.cache.cache(value: data, for: userReference.name)
+            }
+            
+        }
+        
+        let checkReuseOperation = BlockOperation {
+            if let currentIndexPath = self.tableView.indexPath(for: cell),
+                currentIndexPath != indexPath {
+                return
+            }
+            if let imageData = fetchPictureOperation.imageData {
+                cell.imageView?.image = UIImage(data: imageData)
+                cell.textLabel?.text = userReference.name
+            }
+        }
+        
+        cachedOperation.addDependency(fetchPictureOperation)
+            checkReuseOperation.addDependency(fetchPictureOperation)
+        picFetchQueue.addOperation(fetchPictureOperation)
+        picFetchQueue.addOperation(cachedOperation)
+        OperationQueue.main.addOperation(checkReuseOperation)
+        self.operations[userReference.name] = fetchPictureOperation
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+        let userReference = self.networkController.users[indexPath.item]
+        guard let fetchOperation = self.operations[userReference.name]
+            else { NSLog("error cancelling photo request"); return}
+        fetchOperation.cancel()
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ShowDetail" {
+            guard let detailVC = segue.destination as? UserDetailViewController,
+            let index = tableView.indexPathForSelectedRow
+                else {return}
+            detailVC.user = networkController.users[index.row]
+        }
     }
-    */
+ 
 
 }
