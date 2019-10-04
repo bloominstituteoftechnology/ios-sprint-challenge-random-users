e8af7fadff4eb9d39e5554dbdf920df5a8271359
Jordan Christensen <jordan.c4922@gmail.com>
2019-10-05 02:01:04 +0800

    Add model, model controller, function to show images, etc.

---
+++ Random Users/Models/RandomUser.swift
@@ -0,0 +1,39 @@
+//
+//  RandomUser.swift
+//  Random Users
+//
+//  Created by Jordan Christensen on 10/4/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+struct UsersList: Codable {
+    let results: [RandomUser]
+}
+
+struct RandomUser: Codable {
+    let name: Name
+    let email: String
+    let id: ID
+    let phone: String
+    let cell: String
+    let picture: Pictures
+}
+
+struct Name: Codable {
+    let title: String
+    let first: String
+    let last: String
+}
+
+struct Pictures: Codable {
+    let large: String
+    let medium: String
+    let thumbnail: String
+}
+
+struct ID: Codable {
+    let name: String
+    let value: String
+}
--- Random Users/View Controllers/UserDetailViewController.swift
+++ Random Users/View Controllers/UserDetailViewController.swift
@@ -15,12 +15,50 @@ class UserDetailViewController: UIViewController {
     @IBOutlet weak var phoneNumberLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
     
-//    var user: RandomUser?
+    var user: RandomUser?
+    var cache: Cache<String, UIImage>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    private func updateViews() {
+        guard let cache = cache, let user = user, isViewLoaded else { return }
+        
+        if let image = cache.fetch(key: "large\(user.id.value)") {
+            userImageView.image = image
+        } else {
+            guard let imageURL = URL(string: user.picture.large) else { return }
+            let imageRequest = URLRequest(url: imageURL)
+            
+            URLSession.shared.dataTask(with: imageRequest) { (data, _, error) in
+                if let error = error {
+                    NSLog("Error fetching image data: \(error)")
+                    return
+                }
+                
+                guard let data = data else {
+                    NSLog("Error. No data returned from given URL")
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    let image = UIImage(data: data)
+                    self.userImageView.image = image
+                        
+                    if let image = image {
+                        self.cache?.imageDict["large\(user.id.value)"] = image
+                    }
+                }
+            }.resume()
+        }
+        
+        title = "\(user.name.title.uppercased()). \(user.name.last.uppercased())"
+        nameLabel.text = "\(user.name.title.uppercased()). \(user.name.first.uppercased()) \(user.name.last.uppercased())"
+        phoneNumberLabel.text = "Cell: \(user.cell)\nPhone: \(user.phone)"
+        emailLabel.text = "\(user.email)"
     }
     
 
