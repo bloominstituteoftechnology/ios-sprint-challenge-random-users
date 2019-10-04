6bd3d2320b3b9e6e9e384af695fde047ba76cff7
Alexandra Rhodes <anrhodes5738@gmail.com>
2019-10-04 10:11:46 -0500

    Added Project + Completed Storyboard Setup

---
+++ Random Users/View Controllers/PersonDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PersonDetailViewController.swift
+//  Random Users
+//
+//  Created by Alex Rhodes on 10/4/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class PersonDetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
