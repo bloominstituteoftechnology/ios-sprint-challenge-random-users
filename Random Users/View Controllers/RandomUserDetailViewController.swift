3c7ceec2a6f0a9a0985291a5f6bf9ca3a9e59a1a
Ciara Beitel <ciarabeitel@gmail.com>
2019-10-04 11:24:10 -0400

    Create project. Create storyboards and view/table controllers. Hook up outlets and actions.

---
+++ Random Users/View Controllers/RandomUserDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RandomUserDetailViewController.swift
+//  Random Users
+//
+//  Created by Ciara Beitel on 10/4/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class RandomUserDetailViewController: UIViewController {
+    
+    @IBOutlet weak var largeImage: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var phone: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
