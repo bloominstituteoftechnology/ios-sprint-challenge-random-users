a6ffa932909210e1b70a1752c15c008a53cff0c4
Nate Hedgeman <47337337+hedgemanNate@users.noreply.github.com>
2019-10-04 12:08:41 -0400

    added view controllers and view controller files. added custom cell and segues.

---
+++ C/UsersDetailedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UsersDetailedViewController.swift
+//  Random Users
+//
+//  Created by Nathan Hedgeman on 10/4/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UsersDetailedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ V/UserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserTableViewCell.swift
+//  Random Users
+//
+//  Created by Nathan Hedgeman on 10/4/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
