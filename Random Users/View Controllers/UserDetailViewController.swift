04783f6deeb925a21b7ebfa6306ad1e388db1ce8
Jordan Christensen <jordan.c4922@gmail.com>
2019-10-04 23:36:00 +0800

    Set up storyboard, add viewControllers

---
+++ Random Users/View Controllers/UserDetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  UserDetailViewController.swift
+//  Random Users
+//
+//  Created by Jordan Christensen on 10/4/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+    
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+//    var user: RandomUser?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
