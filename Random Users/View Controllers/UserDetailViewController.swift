221c85e2d6f9d0df0d175f53a5d8160b117b6f5a
Jake <jlconnerly@gmail.com>
2019-10-04 10:33:28 -0500

    UI setup complete

---
+++ Random Users/View Controllers/UserDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  UserDetailViewController.swift
+//  Random Users
+//
+//  Created by Jake Connerly on 10/4/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+
+    @IBOutlet weak var userDetailImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Random Users/View Controllers/UserTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  UserTableViewController.swift
+//  Random Users
+//
+//  Created by Jake Connerly on 10/4/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
