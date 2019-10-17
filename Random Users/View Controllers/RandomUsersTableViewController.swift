27197fb17122ae2ec7443bde9ceb7a6a8dd85b8a
ladybeitel <ciarabeitel@gmail.com>
2019-10-17 16:55:37 -0400

    Add block operations and cancel()

--- Random Users/View Controllers/RandomUsersTableViewController.swift
+++ Random Users/View Controllers/RandomUsersTableViewController.swift
@@ -26,6 +26,58 @@ class RandomUsersTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    // MARK: - Methods
+    
+    private func loadImage(forCell cell: RandomUserTableViewCell, forItemAt indexPath: IndexPath) {
+        
+        let user = userController.users[indexPath.row]
+                
+        // Check if there is cached data
+        if let cachedData = cache.value(key: user.picture.thumbnail),
+            let image = UIImage(data: cachedData) {
+            cell.thumbnail.image = image
+            return
+        }
+        
+        // Start fetch operations
+        let fetchOp = FetchPhotoOperation(photoReference: user.picture)
+        
+        // Save data in the cache with key
+        let cacheOp = BlockOperation {
+            if let data = fetchOp.imageData {
+                self.cache.cache(value: data, key: user.picture.thumbnail)
+            }
+        }
+        
+        // Put the image in the row
+        let completitionOp = BlockOperation {
+            defer { self.operations.removeValue(forKey: user.picture.thumbnail) }
+            if let currentIndexPath = self.tableView.indexPath(for: cell),
+                currentIndexPath != indexPath { // reuse cell
+                return
+            }
+            if let data = fetchOp.imageData {
+                cell.thumbnail.image = UIImage(data: data)
+            }
+        }
+        
+        // fetchOp must finish first
+        cacheOp.addDependency(fetchOp)
+        completitionOp.addDependency(fetchOp)
+        
+        photoFetchQueue.addOperation(fetchOp)
+        photoFetchQueue.addOperation(cacheOp)
+        OperationQueue.main.addOperation(completitionOp)
+        
+        operations[user.picture.thumbnail] = fetchOp
+    }
+    
+    override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let user = userController.users[indexPath.row]
+        operations[user.picture.thumbnail]?.cancel()
+    }
+    
 
     // MARK: - Table view data source
 
