d9917f01c6d66246273443000501f1ae17b2f22c
Jerry Haaser <jerry.haaser@hotmail.com>
2019-11-01 11:32:27 -0500

    set up model and model controller

---
+++ Random Users/FetchPhotos/FetchPhotos.swift
@@ -0,0 +1,51 @@
+//
+//  FetchPhotos.swift
+//  Random Users
+//
+//  Created by admin on 11/1/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+class FetchPeoplePhotosOperation: ConcurrentOperation {
+    
+    let person: Person
+    
+    private (set) var imageData: Data?
+    private let session: URLSession
+    private var dataTasks: URLSessionDataTask?
+    
+    init(person: Person, session: URLSession = URLSession.shared) {
+        self.person = person
+        self.session = session
+        super.init()
+    }
+    
+    override func start() {
+        state = .isExecuting
+        
+        guard let url = person.picture.large.usingHTTPS else { return }
+        
+        let task = session.dataTask(with: url) { (data, _, error) in
+            defer { self.state = .isFinished }
+            if self.isCancelled { return }
+            
+            if let error = error {
+                NSLog("Error with data task: \(self.person), \(error)")
+                return
+            }
+            
+            guard let data = data else { return }
+            self.imageData = data
+        }
+        
+        task.resume()
+        dataTasks = task
+    }
+    
+    override func cancel() {
+        dataTasks?.cancel()
+        super.cancel()
+    }
+}
