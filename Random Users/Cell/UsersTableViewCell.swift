9ac9e25d01f2a4706c2b26b7cc99e71c1219fc6e
Jerry Haaser <jerry.haaser@hotmail.com>
2019-11-01 12:46:43 -0500

    set up tableview and detail view

--- Random Users/Cell/UsersTableViewCell.swift
+++ Random Users/Cell/UsersTableViewCell.swift
@@ -13,16 +13,4 @@ class UsersTableViewCell: UITableViewCell {
     @IBOutlet weak var usersImageView: UIImageView!
     @IBOutlet weak var usersNameLabel: UILabel!
     
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
--- Random Users/FetchPhotos/FetchPhotos.swift
+++ Random Users/FetchPhotos/FetchPhotos.swift
@@ -49,3 +49,11 @@ class FetchPeoplePhotosOperation: ConcurrentOperation {
         super.cancel()
     }
 }
+
+extension URL {
+    var usingHTTPS: URL? {
+        guard var components = URLComponents(url: self, resolvingAgainstBaseURL: true) else { return nil }
+        components.scheme = "https"
+        return components.url
+    }
+}
