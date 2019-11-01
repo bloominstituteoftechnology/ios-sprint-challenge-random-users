41c5d1149a719438638a744a3b4278b401f973bf
Jonalynn Masters <jonalynna.masters@gmail.com>
2019-11-01 10:31:33 -0700

    UserTableViewController set up

--- View Controllers/UserTableViewController.swift
+++ View Controllers/UserTableViewController.swift
@@ -9,82 +9,110 @@
 import UIKit
 
 class UserTableViewController: UITableViewController {
-
+    
+// MARK: Properties
+    let userClient = UserClient()
+    let cache = Cache<URL, Data>()
+    var storedFetchOperations: [URL: FetchPhotoOperation] = [:]
+    private let photoFetchQueue = OperationQueue()
+    let queue = DispatchQueue(label: "CancelOperationQueue")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.tableFooterView = UIView()
+        userClient.fetchUsers { (error) in
+            if let error = error {
+                NSLog("Error fetching users: \(error)")
+                return
+            }
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return userClient.users.count
     }
-
-    /*
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath) as? UserTableViewCell else { return UITableViewCell () }
+        let user = userClient.users[indexPath.row]
+        cell.nameLabel.text = "\(user.name.title) \(user.name.first) \(user.name.last)"
+        loadImage(forCell: cell, forItemAt: indexPath)
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let user = userClient.users[indexPath.row]
+        let operation = storedFetchOperations[user.picture.large]
+        queue.sync {
+            operation?.cancel()
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private func loadImage(forCell cell: UserTableViewCell, forItemAt indexPath: IndexPath) {
+        let user = userClient.users[indexPath.row]
+        let photoURL = user.picture.large
+        
+        if let imageData = cache.value(for: photoURL) {
+            let image = UIImage(data: imageData)
+            cell.userImageView.image = image
+            return
+        }
+        
+        let fetchPhotoOperation = FetchPhotoOperation(user: user)
+        
+        let storeDataInCache = BlockOperation {
+            guard let imageData = fetchPhotoOperation.imageData else { return }
+            self.cache.cache(value: imageData, for: photoURL)
+        }
+        
+        let setImageAndName = BlockOperation {
+            defer {
+                self.storedFetchOperations.removeValue(forKey: user.picture.large)
+            }
+            
+            if let currentIndexPath = self.tableView.indexPath(for: cell),
+                currentIndexPath != indexPath {
+                return
+            }
+            
+            guard let imageData = fetchPhotoOperation.imageData else { return }
+            cell.userImageView.image = UIImage(data: imageData)
+        }
+        
+        storeDataInCache.addDependency(fetchPhotoOperation)
+        setImageAndName.addDependency(fetchPhotoOperation)
+        
+        photoFetchQueue.addOperation(fetchPhotoOperation)
+        photoFetchQueue.addOperation(storeDataInCache)
+        OperationQueue.main.addOperation(setImageAndName)
+        
+        storedFetchOperations[user.picture.large] = fetchPhotoOperation
     }
-    */
-
+    
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowDetailSegue" {
+            guard let detailVC = segue.destination as? DetailViewController,
+                let indexPath = tableView.indexPathForSelectedRow else { return }
+            let user = userClient.users[indexPath.row]
+            detailVC.user = user
+            detailVC.userClient = userClient
+        }
+    }
 }
