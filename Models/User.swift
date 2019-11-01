5992231cc981fdcf487f537ea55f866fbb970c87
Wilma <kima_92@fastmail.com>
2019-11-01 12:12:55 -0400

    Configured User model for decoding

--- Models/User.swift
+++ Models/User.swift
@@ -8,10 +8,49 @@
 
 import Foundation
 
-struct User {
-    var id: Int
-    var name: String
-    var email: String
-    var phoneNumber: String
-    var userImage: Data
+struct Users: Codable {
+    
+    var results: [UserResult]
+    
+    struct UserResult: Codable {
+        
+        var name: UserName
+        var email: String
+        var phone: String
+        var picture: ImageSize
+        //var userImage: Data
+        
+        enum UserKeys: String, CodingKey {
+            case name
+            case email
+            case phone
+            case picture
+            
+        }
+        
+        struct UserName: Codable {
+            var title: String
+            var first: String
+            var last: String
+            
+            enum NameKeys: String, CodingKey {
+                case title
+                case first
+                case last
+            }
+        }
+        
+        
+        struct ImageSize: Codable {
+            var picture: URL
+            
+            enum ImageSizeKeys: String, CodingKey {
+                case large
+                case thumbnail
+            }
+        }
+    }
 }
+
+
+
