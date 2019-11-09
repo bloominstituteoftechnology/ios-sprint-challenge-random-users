f9db51dc7508c84473bed56787082473e09e2af2
Vici Shaweddy <vshaweddy@gmail.com>
2019-11-09 11:26:41 -0800

    setting up models and controller

---
+++ Random Users/Controllers/RandomUserController.swift
@@ -0,0 +1,46 @@
+//
+//  RandomUserController.swift
+//  Random Users
+//
+//  Created by Vici Shaweddy on 11/9/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case put = "PUT"
+    case post = "POST"
+    case delete = "DELETE"
+}
+
+class RandomUserController {
+    var users: [RandomUser] = []
+    let baseURL = URL(string: "https://randomuser.me/api/?format=json&inc=name,email,phone,picture&results=1000")!
+    
+    func getUsers(completion: @escaping ([RandomUser]?, Error?) -> Void) {
+        URLSession.shared.dataTask(with: baseURL) { (data, _, error) in
+            guard error == nil else {
+                completion(nil, error)
+                return
+                
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned from data task")
+                completion(nil, error)
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let randomUsers = try decoder.decode([RandomUser].self, from: data)
+                self.users = randomUsers
+                completion(randomUsers, nil)
+            } catch {
+                print("Error: \(error.localizedDescription)")
+            }
+        }.resume()
+    }
+}
---
+++ Random Users/Models/RandomUser.swift
@@ -0,0 +1,16 @@
+//
+//  RandomUser.swift
+//  Random Users
+//
+//  Created by Vici Shaweddy on 11/9/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+struct RandomUser: Codable {
+    let name: Name
+    let email: String
+    let phone: String
+    let picture: Picture
+}
