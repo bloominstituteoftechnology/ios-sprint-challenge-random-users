8e71187317d56a7b0d43e303f3117d363aabf13b
Robert Keffury <bobby.keffury@me.com>
2019-11-09 10:05:34 -0800

    Added search for users method.

--- Random Users/Application/UserController.swift
+++ Random Users/Application/UserController.swift
@@ -13,12 +13,45 @@ enum HTTPMethod: String {
     case get = "GET"
 }
 
+enum NetworkError: Error {
+    case badData
+    case otherError
+    case noDecode
+}
+
 class UserController {
     
     var users: [User] = []
     
     private let baseUrl = URL(string: "https://randomuser.me/api/?format=json&inc=name,email,phone,picture&results=1000")!
     
+    func searchForUsers(completion: @escaping (Result<User, NetworkError>) -> Void) {
+        
+        var request = URLRequest(url: baseUrl)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            
+            if let _ = error {
+                completion(.failure(.otherError))
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            let decoder = JSONDecoder()
+            
+            do {
+                let user = try decoder.decode(User.self, from: data)
+                completion(.success(user))
+                
+            } catch {
+                print("Error decoding User object: \(error)")
+                completion(.failure(.noDecode))
+                return
+            }
+        }.resume()
+    }
     
     
 }
