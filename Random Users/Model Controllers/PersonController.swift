33b2eb2cec40a01876bcca73f60d89182003410c
John Kouris <jdkouris@gmail.com>
2019-11-09 09:41:09 -0800

    Added the getPeople method

--- Random Users/Model Controllers/PersonController.swift
+++ Random Users/Model Controllers/PersonController.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case put = "PUT"
+    case post = "POST"
+    case delete = "DELETE"
+}
+
+class PersonController {
+    
+    static let shared = PersonController()
+    
+    var people: [Person] = []
+    
+    let baseURL = URL(string: "https://randomuser.me/api/?format=json&inc=name,email,phone,picture&results=1000")
+    
+    func getPeople(completion: @escaping (Error?) -> Void) {
+        guard let baseURL = baseURL else { return }
+        
+        URLSession.shared.dataTask(with: baseURL) { (data, _, error) in
+            if let error = error {
+                print("Error fetching people data: \(error.localizedDescription)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                print("Error getting data")
+                completion(error)
+                return
+            }
+            
+            do {
+                let people = try JSONDecoder().decode(Person.self, from: data)
+                self.people.append(people)
+                completion(nil)
+            } catch {
+                print("Error decoding people: \(error)")
+                completion(error)
+            }
+        }.resume()
+        
+    }
+    
+}
