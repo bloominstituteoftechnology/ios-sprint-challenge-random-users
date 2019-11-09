c73d148ee7583c1de6e60bd559e1b4a9661b312e
John Kouris <jdkouris@gmail.com>
2019-11-09 09:56:27 -0800

    Modified the Person model

--- Random Users/Model/Person.swift
+++ Random Users/Model/Person.swift
@@ -10,10 +10,10 @@ import Foundation
 
 
 struct Person: Codable {
-    var name: Name
+    var name: String
     var email: String
     var phone: String
-    var picture: Picture
+    var picture: String
     
     enum PersonCodingKeys: String, CodingKey {
         case name
@@ -22,40 +22,27 @@ struct Person: Codable {
         case picture
     }
     
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: PersonCodingKeys.self)
-        name = try container.decode(Name.self, forKey: .name)
-        email = try container.decode(String.self, forKey: .email)
-        phone = try container.decode(String.self, forKey: .phone)
-        picture = try container.decode(Picture.self, forKey: .picture)
-    }
-}
-
-struct Name: Codable {
-    var first: String
-    var last: String
-    
     enum NameCodingKeys: String, CodingKey {
         case first
         case last
     }
     
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: NameCodingKeys.self)
-        first = try container.decode(String.self, forKey: .first)
-        last = try container.decode(String.self, forKey: .last)
-    }
-}
-
-struct Picture: Codable {
-    var large: String
-    
     enum PictureCodingKeys: String, CodingKey {
         case large
     }
-    
+}
+
+extension Person {
     init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: PictureCodingKeys.self)
-        large = try container.decode(String.self, forKey: .large)
+        let container = try decoder.container(keyedBy: PersonCodingKeys.self)
+        
+        email = try container.decode(String.self, forKey: .email)
+        phone = try container.decode(String.self, forKey: .phone)
+        
+        let nameContainer = try container.nestedContainer(keyedBy: NameCodingKeys.self, forKey: .name)
+        name = try nameContainer.decode(String.self, forKey: .first) + " " + nameContainer.decode(String.self, forKey: .last)
+        
+        let pictureContainer = try container.nestedContainer(keyedBy: PictureCodingKeys.self, forKey: .picture)
+        picture = try pictureContainer.decode(String.self, forKey: .large)
     }
 }
