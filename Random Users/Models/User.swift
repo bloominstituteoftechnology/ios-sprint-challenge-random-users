b0da05fb9b72e41d31da04f384c130cee2eb6918
Robert Keffury <bobby.keffury@me.com>
2019-11-09 11:38:56 -0800

    Cleaned up

--- Random Users/Models/User.swift
+++ Random Users/Models/User.swift
@@ -17,6 +17,10 @@ struct User: Codable {
     let phoneNumber: String
     let thumbnail: URL
     
+    enum ResultsCodingKeys: String, CodingKey {
+        case results
+    }
+    
     enum UserCodingKeys: String, CodingKey {
         case name
         case emailAddress
@@ -35,18 +39,19 @@ struct User: Codable {
     }
     
     init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: UserCodingKeys.self)
+        let container = try decoder.container(keyedBy: ResultsCodingKeys.self)
+        let resultsContainer = try container.nestedContainer(keyedBy: UserCodingKeys.self, forKey: .results)
         
-        let nameContainer = try container.nestedContainer(keyedBy: NameCodingKeys.self, forKey: .name)
+        let nameContainer = try resultsContainer.nestedContainer(keyedBy: NameCodingKeys.self, forKey: .name)
         title = try nameContainer.decode(String.self, forKey: .title)
         first = try nameContainer.decode(String.self, forKey: .first)
         last = try nameContainer.decode(String.self, forKey: .last)
         
-        let pictureContainer = try container.nestedContainer(keyedBy: PictureCodingKeys.self, forKey: .picture)
+        let pictureContainer = try resultsContainer.nestedContainer(keyedBy: PictureCodingKeys.self, forKey: .picture)
         thumbnail = try pictureContainer.decode(URL.self, forKey: .thumbnail)
         
-        emailAddress = try container.decode(String.self, forKey: .emailAddress)
-        phoneNumber = try container.decode(String.self, forKey: .phoneNumber)
+        emailAddress = try resultsContainer.decode(String.self, forKey: .emailAddress)
+        phoneNumber = try resultsContainer.decode(String.self, forKey: .phoneNumber)
         
     }
     
