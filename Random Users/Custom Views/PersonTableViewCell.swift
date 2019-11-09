2a510d7e91736d3053fb4cac4cd5fb219b06e065
dillonp23 <dillonp.lambda@gmail.com>
2019-11-09 09:39:46 -0800

    Add PeopleTableViewController class, PersonDetailViewController class, and PersonTableViewCell class. Wire up outlets.

---
+++ Random Users/Custom Views/PersonTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PersonTableViewCell.swift
+//  Random Users
+//
+//  Created by Dillon P on 11/9/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var personImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
---
+++ Random Users/View Controllers/PersonDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PersonDetailViewController.swift
+//  Random Users
+//
+//  Created by Dillon P on 11/9/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class PersonDetailViewController: UIViewController {
+    
+    @IBOutlet weak var personImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
