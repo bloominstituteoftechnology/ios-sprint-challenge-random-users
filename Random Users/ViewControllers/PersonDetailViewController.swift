870a4b55f70b610418dc1e4394ab652cb4bcc155
John Kouris <jdkouris@gmail.com>
2019-11-09 09:06:55 -0800

    Added the View Controllers for the table view and the detail view

---
+++ Random Users/ViewControllers/PersonDetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PersonDetailViewController.swift
+//  Random Users
+//
+//  Created by John Kouris on 11/9/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class PersonDetailViewController: UIViewController {
+    
+    @IBOutlet weak var personImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    @IBOutlet weak var emailAddressLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
