b44f5fdc4ac6a39959b1bcb6fcb9834377ad9fb5
brian <bfvilchez95@gmail.com>
2019-12-06 09:47:45 -0700

    setting up view controllers

---
+++ Random Users/View Controllers/PersonDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PersonDetailViewController.swift
+//  Random Users
+//
+//  Created by brian vilchez on 12/6/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class PersonDetailViewController: UIViewController {
+    
+    //MARK: - Properties
+    @IBOutlet weak var personImageView: UIImageView!
+    @IBOutlet weak var personTitleLabel: UILabel!
+    @IBOutlet weak var personNameLabel: UILabel!
+    @IBOutlet weak var personNumberLabel: UILabel!
+    @IBOutlet weak var personEmailLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
