5b1eb7600f7ad5139e749691d9ec5b5fcdae02e5
Dan M <djmthedev@gmail.com>
2019-12-06 08:29:25 -0800

    added UserController

---
+++ Random Users/Model Controllers/UserController.swift
@@ -0,0 +1,15 @@
+//
+//  UserController.swift
+//  Random Users
+//
+//  Created by morse on 12/6/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+class UserController {
+    var users: [User] = []
+    
+    
+}
