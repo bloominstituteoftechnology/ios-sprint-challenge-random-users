7f6bdacaa930290c34ac7686649e498e0d4f8482
Skyler Suzuki <skylersuzuk1@gmail.com>
2019-12-06 08:24:21 -0800

    Added viewcontrollers and view files. Set up storyboard. Hooked up all IBOutlets for each viewcontroller.

---
+++ Random Users/View Controllers/UserDetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  UserDetailViewController.swift
+//  Random Users
+//
+//  Created by Lambda_School_Loaner_204 on 12/6/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var pictureImageView: UIIMageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Random Users/Views/UserTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  UserTableViewCell.swift
+//  Random Users
+//
+//  Created by Lambda_School_Loaner_204 on 12/6/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var pictureImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
