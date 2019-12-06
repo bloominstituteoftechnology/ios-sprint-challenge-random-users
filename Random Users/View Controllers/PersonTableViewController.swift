ad6ee7ef9cb1893c870013afe21ead890290326b
brian <bfvilchez95@gmail.com>
2019-12-06 16:35:18 -0700

    finished project

--- Random Users/View Controllers/PersonTableViewController.swift
+++ Random Users/View Controllers/PersonTableViewController.swift
@@ -10,81 +10,95 @@ import UIKit
 
 class PersonTableViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    
+    //MARK: - Properties
+    private var client = PersonClientAPI()
+    var cache: Cache = Cache<String, Data>()
+    private var operations = [String: Operation]()
+    private let photoFetchQueue = OperationQueue()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        client.fetchRequest { (_) in
+            DispatchQueue.main.sync {
+                self.tableView.reloadData()
+            }
+        }
     }
-
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return client.persons.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+      guard let cell = tableView.dequeueReusableCell(withIdentifier: "PersonCell", for: indexPath) as? PersonTableViewCell else { return UITableViewCell()}
+        let person = client.persons[indexPath.row]
+        cell.personNameLabel.text = "\(person.firstName) \(person.lastName)"
+        loadImage(forCell: cell, forRowAt: indexPath)
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "DetailPersonSegue" {
+          guard let indexPath = tableView.indexPathForSelectedRow,
+            let personDetailVC = segue.destination as? PersonDetailViewController else { return }
+                personDetailVC.person = client.persons[indexPath.row]
+        }
     }
-    */
+
+     //private
+    private func loadImage(forCell cell: PersonTableViewCell, forRowAt indexPath: IndexPath) {
+
+
+        let person = client.persons[indexPath.row]
+        if let cachedData = cache.value(forKey: person.id), let image = UIImage(data: cachedData) {
+            cell.personThumbNail.image = image
+            return
+        }
+
+           // TODO: Implement image loading here
+        let fetchOperation = FetchPhotoOperation(photoType: .thumbNail, person: person)
+        let cacheOp = BlockOperation {
+            if let data = fetchOperation.imageData {
+                self.cache.cache(value: data, forKey: person.id)
+            }
+        }
+        
+ let completionOp = BlockOperation {
+         defer {self.operations.removeValue(forKey: person.id)}
+         if let currentIndexpath = self.tableView.indexPath(for: cell),
+             currentIndexpath != indexPath {
+             print("got image for reused image")
+             return
+         }
+         if let data = fetchOperation.imageData {
+             cell.personThumbNail.image = UIImage(data: data)
+         }
+     }
+     
+           cacheOp.addDependency(fetchOperation)
+           completionOp.addDependency(fetchOperation)
+           photoFetchQueue.addOperation(fetchOperation)
+           photoFetchQueue.addOperation(cacheOp)
+           OperationQueue.main.addOperation(completionOp)
+           operations[person.id] = fetchOperation
+        }
+
+
 
 }
