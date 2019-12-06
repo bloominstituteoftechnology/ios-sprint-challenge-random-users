083dcedff9048f7468bec83a1c96303b9b1b1770
Rwoltx <richardx1016@gmail.com>
2019-12-06 13:59:46 -0500

    Nearly finished. Working on the fetch function.

--- Random Users/View + ViewController/RandomUsersTableViewController.swift
+++ Random Users/View + ViewController/RandomUsersTableViewController.swift
@@ -9,87 +9,79 @@
 import UIKit
 
 class RandomUsersTableViewController: UITableViewController {
-
-    let 
     
     
     
+    // MARK: - Properties
+    let client = RandomUsersClient()
+    let cache = Cache<URL, Data>()
+    var fetchOperation: [URL : FetchPhotoOperation] = [:]
+    let photoFetchQueue = OperationQueue()
+    let queue = DispatchQueue(label: "CancelOperationQueue")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        client.fetchRandomUser { ( error ) in
+            if let error = error {
+                NSLog("Error: \(error)")
+                return
+            }
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
-
+    
+    
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return client.savedUsers.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RandomUser", for: indexPath) as? RandomUserTableViewCell else { return UITableViewCell() }
+        
+        let user = client.savedUsers[indexPath.row]
+        let name = "\(user.name.last),\(user.name.first)"
+        cell.randomUserNameLabel.text = name
+        loadImage(forCell: cell, forItemAt: indexPath)
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let user = client.savedUsers[indexPath.row]
+        let operation = fetchOperation[user.picture.large]
+        queue.sync {
+            operation?.cancel()
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    private func loadImage(forCell cell: RandomUserTableViewCell, forItemAt indexPath: IndexPath) {
+        let user = client.savedUsers[indexPath.row]
+        let url = user.picture.large
+        
+        if let imageData = cache.value(for: url) {
+            let image = UIImage(data: imageData)
+            cell.randomUserThumbnailImage.image = image
+            return
+        }
+    
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ShowDetail" {
+            guard let detailVC = segue.destination as? RandomUserDetailViewController,
+                let indexPath = tableView.indexPathForSelectedRow else { return }
+            detailVC.user = client.savedUsers[indexPath.row]
+        }
     }
-    */
-
+    
+    
 }
