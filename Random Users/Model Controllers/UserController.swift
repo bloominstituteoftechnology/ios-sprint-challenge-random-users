e00b664c145d4ab8b96685f1cdf7f65132057776
Dan M <djmthedev@gmail.com>
2019-12-06 08:29:43 -0800

    added fetchUsers()

--- Random Users/Model Controllers/UserController.swift
+++ Random Users/Model Controllers/UserController.swift
@@ -11,5 +11,15 @@ import Foundation
 class UserController {
     var users: [User] = []
     
-    
+    func fetchUsers() {
+        let baseURL = URL(string: "https://randomuser.me/api/?format=json&results=10&noinfo")!
+        
+        do {
+            let data = try Data(contentsOf: baseURL)
+            let people = try JSONDecoder().decode(Results.self, from: data)
+            users = people.results
+        } catch {
+            print("Error fetching poeple: \(error)")
+        }
+    }
 }
