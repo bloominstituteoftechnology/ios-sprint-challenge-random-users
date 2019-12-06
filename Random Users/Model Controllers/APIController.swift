beee1b959a9db40bef8ce05051547983ff4fb12b
Jon Bash <jonbash@protonmail.com>
2019-12-06 08:10:06 -0800

    add APIController file

---
+++ Random Users/Model Controllers/APIController.swift
@@ -0,0 +1,13 @@
+//
+//  APIController.swift
+//  Random Users
+//
+//  Created by Jon Bash on 2019-12-06.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+class APIController {
+    
+}
