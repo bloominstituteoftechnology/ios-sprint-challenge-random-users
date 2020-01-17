d901fb220e133fe3999af436150a037d8fa1b8da
Alex Shillingford <alex.shillingford@icloud.com>
2020-01-17 09:59:11 -0700

    Create Models, Create View Controller files and wire up IBOutlets

---
+++ Random Users/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Random Users
+//
+//  Created by Alex Shillingford on 1/17/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    var title, first, last, email, cell: String
+    var picture: UserImage
+}
