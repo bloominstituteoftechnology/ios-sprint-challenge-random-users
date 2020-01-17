1c88bd8ae43053f9ab038b0171758bf2139c319a
Chad Rutherford <chad_rutherford@outlook.com>
2020-01-17 11:30:16 -0500

    Filled Out UsersTableViewController

--- Random Users/Resources/AppDelegate.swift
+++ Random Users/Resources/AppDelegate.swift
@@ -5,7 +5,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow()
-        
+        let usersTableVC = UsersTableViewController(nibName: nil, bundle: nil)
+        window?.rootViewController = UINavigationController(rootViewController: usersTableVC)
+        window?.makeKeyAndVisible()
         return true
     }
 }
