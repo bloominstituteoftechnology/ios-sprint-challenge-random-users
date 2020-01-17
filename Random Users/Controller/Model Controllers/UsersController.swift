f7cb92429f7f623ca7080a1a44ba611d829c3779
Chad Rutherford <chad_rutherford@outlook.com>
2020-01-17 12:08:29 -0500

    Finished Populating UsersTableViewController with Users

--- Random Users/Controller/Model Controllers/UsersController.swift
+++ Random Users/Controller/Model Controllers/UsersController.swift
@@ -10,4 +10,25 @@ import Foundation
 
 class UsersController {
     
+    // --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    // MARK: - Data Source
+    var users = [User]()
+    let fetchUsersOperation = FetchUsersOperation()
+    let userQueue = OperationQueue()
+    
+    
+    // --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    // MARK: - Setup Functions
+    func fetchUsers(completion: @escaping () -> ()) {
+        let completionOperation = BlockOperation {
+            if let users = self.fetchUsersOperation.users {
+              self.users = users
+            }
+            completion()
+        }
+        
+        userQueue.addOperation(fetchUsersOperation)
+        completionOperation.addDependency(fetchUsersOperation)
+        OperationQueue.main.addOperation(completionOperation)
+    }
 }
