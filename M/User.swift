e131e6640ab1bc2b73010755934ad23f6d335b42
HedgemanNate <nate.hedgeman@gmail.com>
2020-01-17 12:23:38 -0500

    Built UI and created User model

---
+++ M/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  Random Users
+//
+//  Created by Nathan Hedgeman on 1/17/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
---
+++ V/UsersTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UsersTableViewCell.swift
+//  Random Users
+//
+//  Created by Nathan Hedgeman on 1/17/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UsersTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
