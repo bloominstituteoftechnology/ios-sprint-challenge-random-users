684845d088a6a508b7e150f1caf7432a9b3bd524
Christian Lorenzo <clbmiami2004@gmail.com>
2020-01-25 13:45:12 -0800

    Christian Lorenzo - Finished Project. - Added feature to show current user being viewed to show on the detailViewController title.

---
+++ UserTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  UserTableViewCell.swift
+//  Random Users
+//
+//  Created by Lambda_School_Loaner_201 on 1/25/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
--- UsersTableViewController.swift
+++ UsersTableViewController.swift
@@ -20,74 +20,101 @@ class UsersTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         photoFetchQueue.name = "com.RandomeUsers.PhotFectchQueue"
-
+        userController.fetchUsers { (error) in
+            if let error = error {
+                print("Error fetching user: \(error)")
+            }
+            
+            DispatchQueue.main.async {
+                print(self.userController.users.count)
+                self.tableView.reloadData()
+            }
+        }
     }
+    
+    
+    
+    
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        return 0
+//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return userController.users.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath)
+        guard let userCell = cell as? UserTableViewCell else { return cell }
+        
+        let user = userController.users[indexPath.row]
+        userCell.nameLabel?.text = user.name
+        
+        loadImage(forCell: userCell, forItemAt: indexPath)
 
         // Configure the cell...
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        60
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    //MARK: Fuctions:
+    
+    
+    private func loadImage(forCell cell: UserTableViewCell, forItemAt indexPath: IndexPath) {
+        if let imageData = thumbnailCache.value(for: indexPath.row) {
+            cell.userImageView?.image = UIImage(data: imageData)
+        }
+        
+        let user = userController.users[indexPath.row]
+        let fetchPhotoOperation = FetchPhotoOperation(userImageUrl: user.picture[0])
+        
+        let storeToCache = BlockOperation {
+            if let imageData = fetchPhotoOperation.imageData {
+                self.thumbnailCache.cache(value: imageData, for: indexPath.row)
+            }
+        }
+        
+        let cellReuseCheck = BlockOperation {
+            if self.tableView.indexPath(for: cell) == indexPath {
+                guard let imageData = fetchPhotoOperation.imageData else { return }
+                cell.userImageView.image = UIImage(data: imageData)
+            }
+        }
+        
+        storeToCache.addDependency(fetchPhotoOperation)
+        cellReuseCheck.addDependency(fetchPhotoOperation)
+        
+        photoFetchQueue.addOperations([fetchPhotoOperation, storeToCache], waitUntilFinished: false)
+        OperationQueue.main.addOperation(cellReuseCheck)
+        fetchPhotoOperations[indexPath.row] = fetchPhotoOperation
+        //print(fetchPhotoOperations[indexPath.row]!.state)
     }
-    */
+    
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "DetailSegue" {
+            guard let vc = segue.destination as? UserDetailViewController,
+                let indexpath = tableView.indexPathForSelectedRow    else { return }
+            
+            let userIndex = indexpath.row
+            let user = userController.users[userIndex]
+            vc.user = user
+            vc.userController = userController
+            vc.userIndex = userIndex
+        }
     }
-    */
-
 }
