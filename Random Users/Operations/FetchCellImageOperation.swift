e481bef1b9ce559ef779beb3865a6499193ced19
Enayatullah Naseri <enayatullahna@gmail.com>
2020-01-24 19:31:26 -0800

    ConcurrentOperation

---
+++ Random Users/Operations/FetchCellImageOperation.swift
@@ -0,0 +1,14 @@
+//
+//  FetchCellImageOperation.swift
+//  Random Users
+//
+//  Created by Enayatullah Naseri on 1/24/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FetchCellImageOperation: ConcurrentOperation {
+    
+}
---
+++ Random Users/Operations/FetchDetailViewImageOperation.swift
@@ -0,0 +1,14 @@
+//
+//  FetchDetailViewImageOperation.swift
+//  Random Users
+//
+//  Created by Enayatullah Naseri on 1/24/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FetchDetailViewImageOperation: ConcurrentOperation {
+    
+}
