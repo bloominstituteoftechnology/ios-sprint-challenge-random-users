84968c892cd3751a3ccee20b5e57ea5a03a8d6e3
Enayatullah Naseri <enayatullahna@gmail.com>
2020-01-24 20:34:05 -0800

    Table view image and label duplicated

--- Random Users/View Controllers/UsersDetailViewController.swift
+++ Random Users/View Controllers/UsersDetailViewController.swift
@@ -12,7 +12,11 @@ import UIKit
 class UsersDetailViewController: UIViewController {
     
     // Properties
+    var userImage: UIImage?
     var usersController: UsersController?
+    private var cache: Cache<String, UIImage> = Cache()
+    private let operationQueue = OperationQueue()
+    private var fetchOp: [String : FetchDetailViewImageOperation] = [:]
     var user: User? {
         didSet {
             updateViews()
@@ -36,6 +40,45 @@ class UsersDetailViewController: UIViewController {
 
     func updateViews() {
         
+        guard isViewLoaded, let user = user else {return}
+        
+        navigationItem.title = user.name
+        nameLabel.text = user.name
+        phoneNumberLabel.text = user.phone
+        emailLabel.text = user.email
+        
+        userImageView.image = userImage
+        userImageView.contentMode = .scaleAspectFit
+        
+        loadingImage(for: user) // use loading image
+        
+    }
+    
+    // loading image from FetchDetailViewImageOperation
+    private func loadingImage(for randomUsers: User) {
+        
+        if let image = cache.value(key: randomUsers.email) {
+            userImageView.image = image
+        } else {
+            let fetchImageOp = FetchDetailViewImageOperation(user: randomUsers)
+            
+            let cacheOperation = BlockOperation {
+                guard let image = fetchImageOp.largeImage else { return }
+                self.cache.cache(value: image, key: randomUsers.email)
+            }
+            
+            let setImageOp = BlockOperation {
+                guard let image = fetchImageOp.largeImage else { return }
+                self.userImageView.image = image
+            }
+            
+            cacheOperation.addDependency(fetchImageOp)
+            setImageOp.addDependency(fetchImageOp)
+            operationQueue.addOperations([fetchImageOp, cacheOperation], waitUntilFinished: false)
+            OperationQueue.main.addOperation(setImageOp)
+            
+            fetchOp[randomUsers.phone] = fetchImageOp
+        }
     }
 
 }
--- Random Users/View Controllers/UsersTableViewController.swift
+++ Random Users/View Controllers/UsersTableViewController.swift
@@ -13,9 +13,23 @@ class UsersTableViewController: UITableViewController {
 
     // Properties
     let usersController = UsersController()
+    private var cache: Cache<String, UIImage> = Cache()
+    private let operations = OperationQueue()
+    private var fetchImage: [String : FetchCellImageOperation] = [:]
+    var users: [User] = [] {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        DispatchQueue.main.async {
+//            self.tableView.reloadData()
+//        }
     }
 
     // MARK: - Table view data source
@@ -27,21 +41,71 @@ class UsersTableViewController: UITableViewController {
     
     // IBButton Action add
     @IBAction func addBarButton(_ sender: Any) {
-        
+        usersController.fetchUsers { (_) in
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
     
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath) as! UsersTableViewCell // using table view cell class
 
-        // Configure the cell...
+        let randomUsers = usersController.users[indexPath.row] // random user at row
+        cell.textLabel?.text = randomUsers.name // name
+        
+        //image load at cell here
+        loadingImage(forCell: cell, for: indexPath) // use loading image function
 
         return cell
     }
     
     
     override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        // Code
+        if usersController.users.count > 0 { // cancel loading when not at display
+            let user = usersController.users[indexPath.row]
+            fetchImage[user.email]?.cancel()
+        } else {
+            for (_, operation) in fetchImage {
+                operation.cancel()
+            }
+        }
+    }
+    
+    // loading image to the cell function
+    private func loadingImage(forCell cell: UsersTableViewCell, for indexPath: IndexPath) {
+        
+        let user = usersController.users[indexPath.row]
+        
+        if let image = cache.value(key: user.email) {
+            cell.userImageView.image = image
+        } else {
+            let cellViewOperation = FetchCellImageOperation(user: user)
+            
+            let cacheOperation = BlockOperation {
+                guard let image = cellViewOperation.thumbNailImage else {return}
+                self.cache.cache(value: image, key: user.phone)
+            }
+            
+            let cellUseOperation = BlockOperation {
+                guard let image = cellViewOperation.thumbNailImage else {return}
+                
+                if self.tableView.indexPath(for: cell) == indexPath {
+                    cell.userImageView.image = image
+                    cell.imageView?.image = image
+                    self.tableView.reloadData()
+                }
+            }
+            
+            cacheOperation.addDependency(cellViewOperation)
+            cellUseOperation.addDependency(cellViewOperation)
+            
+            operations.addOperations([cellViewOperation, cacheOperation], waitUntilFinished: false)
+            OperationQueue.main.addOperation(cellUseOperation)
+            
+            fetchImage[user.phone] = cellViewOperation
+        }
     }
 
     
@@ -49,8 +113,13 @@ class UsersTableViewController: UITableViewController {
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "toDetailView" {
+            guard let detailVC = segue.destination as? UsersDetailViewController,
+                let indexPath = tableView.indexPathForSelectedRow else {return}
+            let users = usersController.users[indexPath.row]
+            detailVC.usersController = usersController
+            detailVC.user = users
+        }
     }
     
 
