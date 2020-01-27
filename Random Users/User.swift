e67ba5b4b11322e90897eae3748264f48d675940
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2020-01-26 19:53:49 -0500

    Added Files

---
+++ Random Users/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  Random Users
+//
+//  Created by Christy Hicks on 1/26/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
---
+++ Random Users/UserController.swift
@@ -0,0 +1,9 @@
+//
+//  UserController.swift
+//  Random Users
+//
+//  Created by Christy Hicks on 1/26/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
