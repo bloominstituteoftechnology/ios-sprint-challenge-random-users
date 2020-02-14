33c958de8c5a5f900fa2b564b91acb4ec54eab6a
AustinPotts <pottsevolvements@gmail.com>
2020-02-14 11:24:27 -0500

    Storyboard Configured with Subclass Files

---
+++ View Controllers/UserDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  UserDetailViewController.swift
+//  Random Users
+//
+//  Created by Austin Potts on 2/14/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userEmailLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
