3cfdd9c94efb97ca216b25861b1ef2ce67cf8ed2
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-02-14 08:31:01 -0800

    basic scenes/files setup

---
+++ Random Users/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Random Users
+//
+//  Created by Jorge Alvarez on 2/14/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    var user: User? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    func updateViews() {
+        print("updateViews() called")
+        guard isViewLoaded else {return}
+        
+        guard let user = user else {return}
+        nameLabel.text = user.name
+        phoneLabel.text = user.phone
+        emailLabel.text = user.email
+        // imageView.image = UIImage(user.imageURL) // etc ...
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
