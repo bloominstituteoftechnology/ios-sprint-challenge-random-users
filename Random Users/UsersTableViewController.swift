8ec45123bbc4d2601ef68677c25bb91b54b04a2b
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-02-20 13:57:51 -0800

    images wont show

--- Random Users/UsersTableViewController.swift
+++ Random Users/UsersTableViewController.swift
@@ -12,11 +12,12 @@ import UIKit
 
 class UsersTableViewController: UITableViewController {
 
-    // Key used to be Int
+    // MARK: - Properties
+    
     private var operations = [String : Operation]()
-    // Key used to be Int / photoReference.id -> photoReference.results[0].email
-    private let cache = Cache<String, Data>()
-    private let photoFetchQueue = OperationQueue()
+    private var cache = Cache<String, UIImage>()
+    private var photoFetchQueue = OperationQueue()
+    let userController = UserController()
     
     private var photoReferences = [User]() {
         didSet {
@@ -24,120 +25,88 @@ class UsersTableViewController: UITableViewController {
         }
     }
     
-    let userController = UserController()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        userController.fetchUsers { (error) in
+            if let error = error {
+                print("well shit: \(error)")
+            } else {
+                DispatchQueue.main.async {
+                    print("success")
+                    self.tableView.reloadData()
+                }
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        tableView.reloadData()
+        //tableView.reloadData() // ? could mess stuff up
     }
 
     // MARK: - Table view data source
 
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return photoReferences.count
-        //return self.userController.userArray.count
+        return userController.users.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-//        let user = userController.userArray[indexPath.row]
-//        print(user)
-//        print(user.name.first)
-//        cell.textLabel?.text = user.name.first
+        
+        let user = userController.users[indexPath.row]
+        cell.textLabel?.text = user.name
         loadImage(forCell: cell, forItemAt: indexPath)
+        
         return cell
     }
     
     private func loadImage(forCell cell: UITableViewCell, forItemAt indexPath: IndexPath) {
-        let photoReference = photoReferences[indexPath.item]
+        
+        let user = userController.users[indexPath.row]
         
         // Check for image in cache
-        if let cachedImageData = cache.value(for: photoReference.results[0].email),
-            let image = UIImage(data: cachedImageData) {
-            cell.imageView?.image = image
+        if let cachedImage = cache.value(for: user.email) {
+            cell.imageView?.image = cachedImage
             return
         }
         
-        // Start an operation to fetch image data
-        let fetchOp = FetchPhotoOperation(photoReference: photoReference)
-        
-        let cacheOp = BlockOperation {
-            if let data = fetchOp.imageData {
-                self.cache.cache(value: data, for: photoReference.results[0].email)
-            }
-        }
-        
-        let completionOp = BlockOperation {
-            defer { self.operations.removeValue(forKey: photoReference.results[0].email) }
+        else {
             
-            if let currentIndexPath = self.tableView?.indexPath(for: cell),
-                currentIndexPath != indexPath {
-                print("Got image for now-reused cell")
-                return // Cell has been reused
+            // Start an operation to fetch image data
+            let fetchOp = FetchPhotoOperation(user: user)
+            
+            let cacheOp = BlockOperation {
+                if let image = fetchOp.image {
+                    self.cache.cache(value: image, for: user.email)
+                }
             }
             
-            if let data = fetchOp.imageData {
-                cell.imageView?.image = UIImage(data: data)
+            let completionOp = BlockOperation {
+                defer { self.operations.removeValue(forKey: user.email) }
+                
+                if let currentIndexPath = self.tableView?.indexPath(for: cell),
+                    currentIndexPath != indexPath {
+                    print("Got image for now-reused cell")
+                    return // Cell has been reused
+                }
+                
+                if let image = fetchOp.image {
+                    cell.imageView?.image = image
+                }
             }
+            
+            cacheOp.addDependency(fetchOp)
+            completionOp.addDependency(fetchOp)
+            
+            photoFetchQueue.addOperation(fetchOp)
+            photoFetchQueue.addOperation(cacheOp)
+            OperationQueue.main.addOperation(completionOp)
+            
+            operations[user.email] = fetchOp
         }
-        
-        cacheOp.addDependency(fetchOp)
-        completionOp.addDependency(fetchOp)
-        
-        photoFetchQueue.addOperation(fetchOp)
-        photoFetchQueue.addOperation(cacheOp)
-        OperationQueue.main.addOperation(completionOp)
-        
-        operations[photoReference.results[0].email] = fetchOp
     }
-    
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -145,6 +114,5 @@ class UsersTableViewController: UITableViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
 
 }
