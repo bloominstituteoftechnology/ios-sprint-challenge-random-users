7e671e22a7b1d447fd2b57cf13bc4daaf458f432
Enrique Gongora <EGTech24@gmail.com>
2020-03-13 08:51:27 -0700

    Cache Implemented

--- Random Users/Model/Cache.swift
+++ Random Users/Model/Cache.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+class Cache<Key: Hashable, Value> {
+    
+    // MARK: - Variables
+    private var thumbnailDictionary: [Key : Value] = [:]
+    private var largeDictionary: [Key : Value] = [:]
+    private let queue = DispatchQueue(label: "Serial Queue")
+    
+    // MARK: - Functions
+    func thumbnailCache(value: Value, for key: Key) {
+        queue.async {
+            return self.thumbnailDictionary[key] = value
+        }
+    }
+    
+    func thumbnailValue(for key: Key) -> Value? {
+        queue.sync {
+            return thumbnailDictionary[key]
+        }
+    }
+    
+    func largeCache(value: Value, for key: Key) {
+        queue.async {
+            return self.largeDictionary[key] = value
+        }
+    }
+    
+    func largeValue(for key: Key) -> Value? {
+        queue.sync {
+            return largeDictionary[key]
+        }
+    }
+    
+}
