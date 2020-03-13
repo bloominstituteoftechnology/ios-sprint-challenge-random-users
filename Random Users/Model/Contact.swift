3c49b3aa5d84bfe4b9ccac3d9befd54f5b2c36c5
Keri Levesque <kerianne166@gmail.com>
2020-03-13 13:50:44 -0400

    fixing fetching photos

---
+++ Random Users/Model/Contact.swift
@@ -0,0 +1,72 @@
+//
+//  Contact.swift
+//  Random Users
+//
+//  Created by Keri Levesque on 3/13/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+
+struct Results: Decodable {
+    var results: [Contact]
+}
+
+struct Contact: Decodable {
+    var name: String
+    var email: String
+    var phone: String
+    var picture: [URL]
+    var thumbnailImage: URL
+    var largeImage: URL
+    
+    enum ContactKeys: String, CodingKey {
+        case name
+        case email
+        case phone
+        case picture
+        
+        enum NameKeys: String, CodingKey {
+            case title
+            case first
+            case last
+        }
+        
+        enum PictureKeys: String, CodingKey {
+            case large
+            case medium
+            case thumbnail
+        }
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: ContactKeys.self)
+        
+        let nameContainer = try container.nestedContainer(keyedBy: ContactKeys.NameKeys.self, forKey: .name)
+        
+        let title = try nameContainer.decode(String.self, forKey: .title)
+        let first = try nameContainer.decode(String.self, forKey: .first)
+        let last = try nameContainer.decode(String.self, forKey: .last)
+        
+        let pictureContainer = try container.nestedContainer(keyedBy: ContactKeys.PictureKeys.self, forKey: .picture)
+        
+        var pictureURLs: [URL] = []
+        
+        let large = try pictureContainer.decode(String.self, forKey: .large)
+        let medium = try pictureContainer.decode(String.self, forKey: .medium)
+        let thumbnail = try pictureContainer.decode(String.self, forKey: .thumbnail)
+        if let largeURL = URL(string: large), let mediumURL = URL(string: medium), let thumbnailURL = URL(string: thumbnail) {
+            pictureURLs.append(largeURL)
+            pictureURLs.append(mediumURL)
+            pictureURLs.append(thumbnailURL)
+        }
+        
+        name = "\(title) \(first) \(last)"
+        email = try container.decode(String.self, forKey: .email)
+        phone = try container.decode(String.self, forKey: .phone)
+        picture = pictureURLs
+        thumbnailImage = URL(string: thumbnail)!
+        largeImage = URL(string: large)!
+    }
+}
---
+++ Random Users/View Controllers/ContactDetailViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ContactDetailViewController.swift
+//  Random Users
+//
+//  Created by Keri Levesque on 3/13/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class ContactDetailViewController: UIViewController {
+    
+    typealias CompletionHandler = (Error?) -> Void
+    
+    var cache = Cache<String, Data>()
+    
+    var contact: Contact? {
+        didSet {
+            
+        }
+    }
+    
+    @IBOutlet weak var contactImageView: UIImageView!
+    @IBOutlet weak var contactNameLabel: UILabel!
+    @IBOutlet weak var contactPhoneLabel: UILabel!
+    @IBOutlet weak var contactEmailLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateViews()
+        loadLargeImage()
+    }
+    
+    func updateViews() {
+        guard let contact = contact else { return }
+        contactNameLabel.text = contact.name
+        contactPhoneLabel.text = contact.phone
+        contactEmailLabel.text = contact.email
+    
+    }
+    
+    func loadLargeImage() {
+        guard let contact = contact else { return }
+        let key = contact.name
+        let largeURL = contact.largeImage
+        let request = URLRequest(url: largeURL)
+        
+        if let cachedData = cache.largeValue(for: key) {
+            let image = UIImage(data: cachedData)
+            DispatchQueue.main.async {
+                self.contactImageView.image = image
+            }
+        } else {
+            URLSession.shared.dataTask(with: request) { data, _, error in
+                if let error = error {
+                    NSLog("Error fetching Large Image data: \(error)")
+                    return
+                }
+                guard let data = data else {
+                    NSLog("No or Bad data for Large Image")
+                    return
+                }
+                self.cache.largeCache(value: data, for: key)
+                DispatchQueue.main.async {
+                    self.contactImageView.image = UIImage(data: data)
+                }
+            }.resume()
+        }
+    }
+}
+
