81cba4e402283bca40838492e6caccba9bafc295
Enrique Gongora <EGTech24@gmail.com>
2020-03-13 09:33:39 -0700

    Completed TableViewCell and most of the table view controller

--- Random Users/View Controllers/UserTableViewController.swift
+++ Random Users/View Controllers/UserTableViewController.swift
@@ -10,17 +10,51 @@ import UIKit
 
 class UserTableViewController: UITableViewController {
 
+    // MARK: - Variables
+    let userController = UserController()
+    let photoFetchQueue = OperationQueue()
+    var operations: [String : Operation] = [:]
+    var cache = Cache<String, Data>()
+    
+    // MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        userController.fetchUser { _ in
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return userController.users.count
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath) as? UserTableViewCell else { return UITableViewCell() }
+        let user = userController.users[indexPath.row]
+        // more goes here
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let user = userController.users[indexPath.row]
+        let key = user.name
+        operations[key]?.cancel()
+    }
+    
+    // MARK: - Functions
+    
+    
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "UserDetailSegue" {
+            guard let userDetailVC = segue.destination as? UserDetailViewController, let indexPath = tableView.indexPathForSelectedRow else { return }
+            let user = userController.users[indexPath.row]
+            // final line goes here
+        }
+    }
+    
 }
