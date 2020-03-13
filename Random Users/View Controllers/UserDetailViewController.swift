4e87578b2b4860fba39bce0390412dcded12cc70
Nick <ptnguyen1901@gmail.com>
2020-03-13 11:12:44 -0400

    Initial commit

---
+++ Random Users/View Controllers/UserDetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  UserDetailViewController.swift
+//  Random Users
+//
+//  Created by Nick Nguyen on 3/13/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+
+    
+    @IBOutlet weak var userImageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var userPhoneLabel: UILabel!
+    
+    @IBOutlet weak var userEmailLabel: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ Random Users/View Controllers/UsersTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  UsersTableViewController.swift
+//  Random Users
+//
+//  Created by Nick Nguyen on 3/13/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UsersTableViewController: UITableViewController {
+
+    
+    
+    
+    
+    
+    @IBAction func addTapped(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+ 
+    
+    
+    
+
+}
