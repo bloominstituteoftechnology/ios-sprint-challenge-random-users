21ceab5801539df64acab9f9b9a9d658d5a4330f
Enrique Gongora <EGTech24@gmail.com>
2020-03-13 08:27:30 -0700

    Storyboard Setup Complete

---
+++ Random Users/View Controllers/UserDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  UserDetailViewController.swift
+//  Random Users
+//
+//  Created by Enrique Gongora on 3/13/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Random Users/View Controllers/UserTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  UserTableViewController.swift
+//  Random Users
+//
+//  Created by Enrique Gongora on 3/13/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
