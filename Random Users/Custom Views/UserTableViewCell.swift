8d6382d2227f00787e7dff8930b92c6fc2175b49
Jessie <jessie.griffin@icloud.com>
2020-03-20 21:22:46 -0400

    Adds ViewControllers to Storyboard and corresponding files

---
+++ Random Users/Custom Views/UserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserTableViewCell.swift
+//  Random Users
+//
+//  Created by Jessie Ann Griffin on 3/20/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
