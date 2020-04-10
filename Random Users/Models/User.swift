8cc8e7f7081cb4814536c26dfc895f80a77f6886
Wyatt Harrell <wyattaharrell@gmail.com>
2020-04-10 11:07:03 -0400

    project setup

---
+++ Random Users/Models/User.swift
@@ -0,0 +1,60 @@
+//
+//  User.swift
+//  Random Users
+//
+//  Created by Wyatt Harrell on 4/10/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+
+
+
+
+
+/*
+ 
+ {
+   "results": [
+     {
+       "name": {
+         "title": "Mademoiselle",
+         "first": "Bettina",
+         "last": "Joly"
+       },
+       "email": "bettina.joly@example.com",
+       "phone": "078 901 27 91",
+       "picture": {
+         "large": "https://randomuser.me/api/portraits/women/5.jpg",
+         "medium": "https://randomuser.me/api/portraits/med/women/5.jpg",
+         "thumbnail": "https://randomuser.me/api/portraits/thumb/women/5.jpg"
+       }
+     },
+     {
+       "name": {
+         "title": "Mrs",
+         "first": "Caroline",
+         "last": "Lambert"
+       },
+       "email": "caroline.lambert@example.com",
+       "phone": "031-469-1485",
+       "picture": {
+         "large": "https://randomuser.me/api/portraits/women/59.jpg",
+         "medium": "https://randomuser.me/api/portraits/med/women/59.jpg",
+         "thumbnail": "https://randomuser.me/api/portraits/thumb/women/59.jpg"
+       }
+     }
+   ],
+   "info": {
+     "seed": "b48ec4170f518225",
+     "results": 2,
+     "page": 1,
+     "version": "1.3"
+   }
+ }
+ 
+*/
