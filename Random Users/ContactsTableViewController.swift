3bd68c30d60b51016fb4e3b961e99e786bbe9b4c
Tobi Tselios <tobi.tselios@gmail.com>
2020-04-10 18:24:48 +0100

    Operations added to Fetch Images

--- Random Users/ContactsTableViewController.swift
+++ Random Users/ContactsTableViewController.swift
@@ -16,6 +16,10 @@ class ContactsTableViewController: UITableViewController {
     
     var cache = Cache<String, Data>()
     
+    var operations: [String: Operation] = [:]
+    
+    let photoFetchQueue = OperationQueue()
+    
     // MARK: - View Lifecycle
 
     override func viewDidLoad() {
@@ -45,6 +49,32 @@ class ContactsTableViewController: UITableViewController {
                 }
             }
         }
+        
+        let fetchPhotoOperation = FetchPhotoOperation(contact: contact)
+        
+        let storedImageOperation = BlockOperation {
+            if let data = fetchPhotoOperation.imageData {
+                self.cache.cache(value: data, for: contact.name)
+            }
+        }
+        
+        let completionOperation = BlockOperation {
+            if let _ = self.tableView.indexPath(for: cell) {
+                if let data = fetchPhotoOperation.imageData {
+                    cell.imageView?.image = UIImage(data: data)
+                } else {
+                    return
+                }
+            }
+        }
+        
+        storedImageOperation.addDependency(fetchPhotoOperation)
+        completionOperation.addDependency(fetchPhotoOperation)
+        photoFetchQueue.addOperation(fetchPhotoOperation)
+        photoFetchQueue.addOperation(storedImageOperation)
+        OperationQueue.main.addOperation(completionOperation)
+        
+        operations[contact.name] = fetchPhotoOperation
     }
 
     // MARK: - Table view data source
@@ -52,12 +82,18 @@ class ContactsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return contactController.contacts.count
     }
+    
+    override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let contact = contactController.contacts[indexPath.row]
+        operations[contact.name]?.cancel()
+    }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath) as? ContactTableViewCell else {
             return UITableViewCell()
         }
-
+        
+        loadImage(forCell: cell, forItemAt: indexPath)
         let contact = contactController.contacts[indexPath.row]
         cell.contact = contact
 
