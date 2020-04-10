42aa0628c1d3ef3cd130b28316d64271f88eb5f0
Brad <braddiroff@icloud.com>
2020-04-10 13:18:55 -0400

    Working around the results issue in the Decoder

--- Random Users/Model/Face.swift
+++ Random Users/Model/Face.swift
@@ -9,44 +9,55 @@
 import Foundation
 
 class Face: Codable {
- //   var name: String
+
+    var name: String
     var phone: String
     var email: String
     var picture: String
     var thumbnail: String
     
-    enum FaceKeys: String, CodingKey {
-        case name
-        case email
-        case phone
-        case picture
+    enum ResultsKeys: String, CodingKey {
         
-        enum NameKeys: String, CodingKey {
-            case title
-            case first
-            case last
-        }
+        case results
         
-        enum PictureKeys: String, CodingKey {
-            case medium
-            case thumbnail
+        enum FaceKeys: String, CodingKey {
+            case name
+            case email
+            case phone
+            case picture
+            
+            enum NameKeys: String, CodingKey {
+                case title
+                case first
+                case last
+            }
+            
+            enum PictureKeys: String, CodingKey {
+                case medium
+                case thumbnail
+            }
         }
     }
+
     
     required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: FaceKeys.self)
+        let bigContainer = try decoder.container(keyedBy: ResultsKeys.self)
         
-  //      let nameContainer = try container.nestedContainer(keyedBy: FaceKeys.NameKeys.self, forKey: .name)
-   //     let first = try nameContainer.decode(String.self, forKey: .first)
-    //    let last = try nameContainer.decode(String.self, forKey: .last)
-  //      name = "\(first) \(last)"
+        var resultsContainer = try bigContainer.nestedUnkeyedContainer(forKey: .results)
         
-        phone = try container.decode(String.self, forKey: .phone)
-        email = try container.decode(String.self, forKey: .email)
-        
-        let picContainer = try container.nestedContainer(keyedBy: FaceKeys.PictureKeys.self, forKey: .picture)
-        picture = try picContainer.decode(String.self, forKey: .medium)
-        thumbnail = try picContainer.decode(String.self, forKey: .thumbnail)
+        while !resultsContainer.isAtEnd {
+            let nameContainer = try container.nestedContainer(keyedBy: ResultsKeys.FaceKeys.NameKeys.self, forKey: .name)
+            let first = try nameContainer.decode(String.self, forKey: .first)
+            let last = try nameContainer.decode(String.self, forKey: .last)
+            name = "\(first) \(last)"
+            
+            phone = try container.decode(String.self, forKey: .phone)
+            email = try container.decode(String.self, forKey: .email)
+            
+            let picContainer = try container.nestedContainer(keyedBy: FaceKeys.PictureKeys.self, forKey: .picture)
+            picture = try picContainer.decode(String.self, forKey: .medium)
+            thumbnail = try picContainer.decode(String.self, forKey: .thumbnail)
+        }
     
     }
 }
