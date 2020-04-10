1f2d883b04cbcaa7b75c4c35cbd848913f9d0f8b
Hector L <hectoremil21@hotmail.com>
2020-04-10 11:57:28 -0400

    Working on fetching from API

---
+++ Random Users/Models/Results.swift
@@ -0,0 +1,13 @@
+//
+//  Results.swift
+//  Random Users
+//
+//  Created by Karen Rodriguez on 4/10/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+class Results: Codable {
+    var results: [Person]
+}
---
+++ Random Users/Networking/PeopleController.swift
@@ -0,0 +1,22 @@
+//
+//  PeopleController.swift
+//  Random Users
+//
+//  Created by Karen Rodriguez on 4/10/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+class PeopleController {
+    var people: [Person] = []
+    let baseURL: URL = URL(string: "https://randomuser.me/api/?format=json&inc=name,email,phone,picture&results=10")!
+    func fetchPeople() {
+        URLSession.shared.dataTask(with: baseURL) { d, r, e in
+            if let error = e {
+                NSLog("Error fetching people : \(error)")
+            }
+        }
+    }
+    
+}
