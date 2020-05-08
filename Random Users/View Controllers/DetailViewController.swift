db5c0b5179d0a1158b38872b799b16271777bdda
Shawn James <shawn.james@me.com>
2020-05-08 11:27:12 -0400

    Storyboard is hooked up to files

---
+++ Random Users/View Controllers/DetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailViewController.swift
+//  Random Users
+//
+//  Created by Shawn James on 5/8/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var largeImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+
+}
---
+++ Random Users/Views/TableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TableViewCell.swift
+//  Random Users
+//
+//  Created by Shawn James on 5/8/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
