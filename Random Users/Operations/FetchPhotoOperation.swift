c259026b65213e556c5d396d43ccca8dbe6b22aa
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-05-08 10:30:50 -0700

    Added image fetching to the operation

--- Random Users/Operations/FetchPhotoOperation.swift
+++ Random Users/Operations/FetchPhotoOperation.swift
@@ -12,9 +12,38 @@ class FetchPhotoOperation: ConcurrentOperation {
     var imageURL: URL
     var imageData: UIImage?
     private var dataTask = URLSessionDataTask()
-    
+    typealias CompletionHandler = (Result<UIImage, NetworkError>) -> Void
     init(imageURL: URL, imageData: UIImage? = nil) {
         self.imageURL = imageURL
         self.imageData = imageData
+    }
+    
+    
+    func fetchImage(to imageURL: URL, completion: @escaping CompletionHandler) {
+        
+        var requestURL = URLRequest(url: imageURL)
+        requestURL.httpMethod = HTTPMethod.get.rawValue
+        
+        dataTask = URLSession.shared.dataTask(with: requestURL, completionHandler: { data, response, error in
+            if let error = error {
+            NSLog("Error in getting the image: \(error)")
+                completion(.failure(.otherNetworkError))
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("Error in getting data")
+                completion(.failure(.badData))
+                return
+            }
+            
+            guard let image = UIImage(data: data) else {
+               NSLog("Error in getting imagedata")
+                completion(.failure(.badData))
+                return
+            }
+            completion(.success(image))
+            })
+        dataTask.resume()
     }
 }
