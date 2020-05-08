8b073002352021589b809784b79314a7a30a5a17
Mark Poggi <markp@mac.com>
2020-05-08 11:09:36 -0700

    Added Method for fetching and deleting Users dynamically.  Still working on bugs.

--- Random Users/Operations/ConcurrentOperation.swift
+++ Random Users/Operations/ConcurrentOperation.swift
@@ -62,3 +62,39 @@ class ConcurrentOperation: Operation {
     }
     
 }
+
+class FetchUserOperation: ConcurrentOperation {
+
+    let photo: Picture
+
+    private (set) var imageData: Data?  // made this a setter.
+
+    // set these up to simplify the start and cancel functions.
+    private let session: URLSession  // get the URL for that pic
+    private var dataTask: URLSessionDataTask? // look for some data
+
+    init(photo: Picture, session: URLSession = URLSession.shared) {
+        self.photo = photo
+        self.session = session
+        super.init()
+    }
+
+    override func start() {
+        state = .isExecuting
+        guard let imageURL = photo.thumbnail.usingHTTPS else { return } // pulled from Picture struct and added the secure URL.
+        let task = session.dataTask(with: imageURL) { (data, _, error) in
+            defer { self.state = .isFinished }
+            if let error = error {
+                NSLog("Error fetching data for \(self.photo), \(error)")
+            }
+            guard let data = data else { return }
+            self.imageData = data
+        }
+        task.resume()
+
+    }
+
+    override func cancel() {
+        dataTask?.cancel()
+    }
+}
--- UsersTableViewController.swift
+++ UsersTableViewController.swift
@@ -10,49 +10,82 @@ import UIKit
 
 class UsersTableViewController: UITableViewController {
 
-      let userClient = UserClient()
+    let userClient = UserClient()
+    private let userDataCache = Cache<String, Data>() // Name and related photo data.
+    private let userFetchQueue = OperationQueue() // OperationQueue for fetch in background.
+    private var operations = [String : Operation]()  // holding each Operation in a dictionary.
 
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            userClient.fetchUsers() { (error) in
-                if let error = error {
-                    NSLog("Error performing data task: \(error)")
-                }
-                DispatchQueue.main.async {
-                    self.tableView.reloadData()
-                }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        userClient.fetchUsers() { (error) in
+            if let error = error {
+                NSLog("Error performing data task: \(error)")
+            }
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
             }
         }
+    }
 
-        // MARK: - Table view data source
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return userClient.users.count
+    }
 
-        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return userClient.users.count
-        }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath)
 
-        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath)
-            let user = userClient.users[indexPath.row]
-            cell.textLabel?.text = user.name.first.capitalized + " " + user.name.last.capitalized
-            guard let imageData = try? Data(contentsOf: user.picture.thumbnail) else { fatalError() }
-            cell.imageView?.image = UIImage(data: imageData)
+        let userReference = userClient.users[indexPath.row]
+
+        if let cachedData = userDataCache.value(key: userReference.email),
+            let image = UIImage(data: cachedData) { cell.imageView?.image = image
+            cell.textLabel?.text = userReference.name.first.capitalized + " " + userReference.name.last.capitalized
+            return cell
+
+        } else {
+            let fetchOperation = FetchUserOperation(photo: userReference.picture)
+            let cacheOperation = BlockOperation {
+                if let data = fetchOperation.imageData {
+                    self.userDataCache.cache(key: userReference.email, value: data)
+                }
+            }
+
+            let displayImageOperation = BlockOperation {
+                defer {self.operations.removeValue(forKey: userReference.email)}
+            }
+            if let data = fetchOperation.imageData {
+                cell.imageView?.image = UIImage(data: data)
+                cell.textLabel?.text = userReference.name.first.capitalized + " " + userReference.name.last.capitalized
+            }
+            userFetchQueue.addOperation(fetchOperation) // fetching operation
+            userFetchQueue.addOperation(cacheOperation)
+            cacheOperation.addDependency(fetchOperation) // can't cache until we have an image to cache
+            displayImageOperation.addDependency(fetchOperation)
+            OperationQueue.main.addOperation(displayImageOperation) // moving our finished images to the main queue
+
+            operations[userReference.email] = fetchOperation
             return cell
         }
-
-
-        // MARK: - Navigation
-
-        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            if segue.identifier == "UserDetail" {
-                guard let userDetailVC = segue.destination as? UserDetailViewController else { return }
-                guard let indexPath = tableView.indexPathForSelectedRow else { return }
-                let user = userClient.users[indexPath.row]
-                userDetailVC.user = user
-            }
-        }
-
-
     }
+
+    override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+
+        //TODO set up cancellation.
+
+        let userReference = userClient.users[indexPath.row]
+    }
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "UserDetail" {
+            guard let userDetailVC = segue.destination as? UserDetailViewController else { return }
+            guard let indexPath = tableView.indexPathForSelectedRow else { return }
+            let user = userClient.users[indexPath.row]
+            userDetailVC.user = user
+        }
+    }
+}
 
 
 
