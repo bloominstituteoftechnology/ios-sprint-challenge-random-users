3fe9ac91455a7adff480f244485b8111d616159d
Brad <braddiroff@icloud.com>
2020-05-08 12:35:58 -0400

    finished project

--- secondOne/Random Users/Controller/FaceController.swift
+++ secondOne/Random Users/Controller/FaceController.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+
+class FaceController {
+    
+    private let baseUrl = URL(string: "https://randomuser.me/api/?format=json&inc=name,email,phone,picture&results=1000")!
+    
+    var results: Results?
+    
+    func getYourFace(completion: @escaping ((Error?) -> Void) = { _ in}) {
+        
+        var request = URLRequest(url: baseUrl)
+         request.httpMethod = "GET"
+         
+         URLSession.shared.dataTask(with: request) { data, response, error in
+        
+            if let error = error {
+                print(error)
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned from data task")
+                completion(NSError())
+                return
+            }
+            
+            do {
+                let faceTime = try JSONDecoder().decode(Results.self, from: data)
+                self.results = faceTime
+                completion(nil)
+            } catch {
+                print(error)
+                NSLog("Error decoding: \(error)")
+                completion(error)
+            }
+            
+            }.resume()
+    }
+    
+}
+
--- secondOne/Random Users/ViewControllers/PictureTableViewController.swift
+++ secondOne/Random Users/ViewControllers/PictureTableViewController.swift
@@ -10,81 +10,103 @@ import UIKit
 
 class PictureTableViewController: UITableViewController {
 
+    let faceController = FaceController()
+    
+    private var cache = Cache<Int, Data>()
+    private var photoFetchQueue = OperationQueue()
+    private var operations = [Int : Operation]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        faceController.getYourFace { (_) in
+             DispatchQueue.main.async {
+                 self.tableView.reloadData()
+             }
+         }
+        
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+        override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        guard let count = faceController.results?.results.count else {return 0}
+        return count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? PictureTableViewCell else {return UITableViewCell()}
 
-        // Configure the cell...
+        let face = faceController.results?.results[indexPath.row]
+        faceController.results?.results[indexPath.row].id = indexPath.row
+        
+        cell.nameLabel.text = face?.name
+        loadImage(forCell: cell, forItemAt: indexPath)
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if let operation = operations[indexPath.row] {
+            operation.cancel()
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+     private func loadImage(forCell cell: PictureTableViewCell, forItemAt indexPath: IndexPath) {
+        guard let face = faceController.results?.results[indexPath.row] else {return}
+        
+        // Check for image in cache
+        if let cachedImageData = cache.value(for: indexPath.row),
+            let image = UIImage(data: cachedImageData) {
+            cell.facePic.image = image
+            return
+        }
+        
+        // Start an operation to fetch image data
+        let fetchOp = FetchPhotoOperation(face: face)
+        let cacheOp = BlockOperation {
+            if let data = fetchOp.imageData {
+                self.cache.cache(value: data, for: indexPath.row)
+            }
+        }
+        let completionOp = BlockOperation {
+            
+            if let currentIndexPath = self.tableView.indexPath(for: cell),
+                currentIndexPath != indexPath {
+                return
+            }
+            
+            if let data = fetchOp.imageData {
+                cell.facePic.image = UIImage(data: data)
+            }
+            self.operations.removeValue(forKey: indexPath.row)
+        }
+        
+        cacheOp.addDependency(fetchOp)
+        completionOp.addDependency(fetchOp)
+        
+        photoFetchQueue.addOperation(fetchOp)
+        photoFetchQueue.addOperation(cacheOp)
+        OperationQueue.main.addOperation(completionOp)
+        
+        operations[face.id] = fetchOp
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+  
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "detailSegue" {
+            
+            guard let vc = segue.destination as? DetailViewController, let theRow = tableView.indexPathForSelectedRow?.row else { return }
+            
+         //   cache.clearData()
+            
+            let newFace = faceController.results?.results[theRow]
+            vc.face = newFace
+            
+        }
     }
-    */
+
 
 }
