50287b67f210b94975b2e2762ab870fe07fe3681
Harmony Radley <harmonyradley99@gmail.com>
2020-05-08 11:52:59 -0400

    API Controller

---
+++ Random Users/APIController.swift
@@ -0,0 +1,16 @@
+//
+//  APIController.swift
+//  Random Users
+//
+//  Created by Harmony Radley on 5/8/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Typealias
+
+private let baseURL = URL(string: "https://randomuser.me/api/?format=json&inc=name,email,phone,picture&results=1000")!
+
+
+
