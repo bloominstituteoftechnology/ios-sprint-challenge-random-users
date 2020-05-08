23522d7c4ba94e8f1e9b142769ef475e11b76d2a
Chris Dobek <dobek.christopher@gmail.com>
2020-05-08 11:38:30 -0400

    Storyboard and UserTableViewCell Created

---
+++ Random Users/Views/UserTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  UserTableViewCell.swift
+//  Random Users
+//
+//  Created by Chris Dobek on 5/8/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var userImage: UIImageView!
+  @IBOutlet weak var userName: UILabel!
+
+  override func prepareForReuse() {
+      userImage.image = #imageLiteral(resourceName: "Lambda_Logo_Full")
+      userName.text = "User Name"
+      super.prepareForReuse()
+  }
+
+}
