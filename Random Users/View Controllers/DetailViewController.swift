775b5f43e42ddfd7e49ce6732a26888e0ba57f82
Shawn James <shawn.james@me.com>
2020-05-08 13:36:44 -0400

    MVP

--- Random Users/View Controllers/DetailViewController.swift
+++ Random Users/View Controllers/DetailViewController.swift
@@ -9,25 +9,36 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateViews()
-    }
-    
-    var user: Result?
-    
-    private func updateViews() {
-        guard let user = user else { return }
-        // TODO: add large image
-        nameLabel.text = "\(user.name.title.rawValue) \(user.name.first) \(user.name.last)"
-        phoneLabel.text = user.phone
-        emailLabel.text = user.email
     }
     
     @IBOutlet weak var largeImage: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var phoneLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
-
+    
+    var user: Result?
+    
+    private func updateViews() {
+        guard let user = user else { return }
+        loadImage(user: user)
+        nameLabel.text = "\(user.name.title.rawValue) \(user.name.first) \(user.name.last)"
+        phoneLabel.text = user.phone
+        emailLabel.text = user.email
+    }
+    
+    private func loadImage(user: Result) {
+        guard let imageURL = URL(string: user.picture.large) else { return }
+        DispatchQueue.global().async {
+            guard let imageData = try? Data(contentsOf: imageURL) else { return }
+            let image = UIImage(data: imageData)
+            DispatchQueue.main.async {
+                self.largeImage.image = image
+            }
+        }
+    }
+    
 }
