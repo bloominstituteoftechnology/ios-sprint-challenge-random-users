350108b6efe000e8a9a2e0823cdbaab8b79a9b53
Chris Dobek <dobek.christopher@gmail.com>
2020-05-08 12:35:39 -0400

    FetchUsersOperations class added to ConcurrentOperations

--- Random Users/Operations/ConcurrentOperation.swift
+++ Random Users/Operations/ConcurrentOperation.swift
@@ -66,4 +66,41 @@ class ConcurrentOperation: Operation {
 class FetchUsersOperation: ConcurrentOperation {
     private(set) var imageData: Data?
     
+    let user: User
+
+       init(user: User) {
+           self.user = user
+       }
+
+       private var dataTask: URLSessionDataTask?
+       override func start() {
+           super.start()
+
+           state = .isExecuting
+           let url = user.picture
+           let task = URLSession.shared.dataTask(with: url) {(data,_,error) in
+
+               defer {self.state = .isFinished}
+               if let error = error {
+                   NSLog("fetch operation error: \(error)")
+                   return
+               }
+               if let data = data {
+                   self.imageData = data
+               }
+
+           }
+           task.resume()
+           dataTask = task
+       }
+
+       override func cancel() {
+           super.cancel()
+
+           if self.isCancelled {
+               if let task = dataTask {
+                   task.cancel()
+               }
+           }
+       }
 }
