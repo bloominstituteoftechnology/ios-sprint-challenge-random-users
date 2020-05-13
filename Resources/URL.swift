f00eaf62a2f212fb1681b9bcbab9810c509a09e8
Harmony Radley <harmonyradley99@gmail.com>
2020-05-13 15:27:23 -0400

    MVP

--- Resources/URL.swift
+++ Resources/URL.swift
@@ -7,3 +7,11 @@
 //
 
 import Foundation
+
+extension URL {
+    var usingHTTPS: URL? {
+        guard var components = URLComponents(url: self, resolvingAgainstBaseURL: true) else { return nil }
+        components.scheme = "https"
+        return components.url
+    }
+}
