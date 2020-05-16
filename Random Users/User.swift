6f2d0d9e8a112919ccd1ee7ecfe6482821823fed
kensam1 <samarakodykennedy@gmail.com>
2020-05-16 00:01:52 -0400

    Setup a User Controller

---
+++ Random Users/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Random Users
+//
+//  Created by Lambda_School_Loaner_241 on 5/15/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+struct User: Equatable, Decodable {
+    let name: String
+    let phoneNumber: String
+    let email: String
+
+}
---
+++ Random Users/UserController.swift
@@ -0,0 +1,78 @@
+//
+//  UserController.swift
+//  Random Users
+//
+//  Created by Lambda_School_Loaner_241 on 5/15/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+let baseURL = URL(string: "https://randomuser.me/api/?format=json&inc=name,email,phone,picture&results=1000")
+
+class UserController {
+    var savedUser: [User] = []
+    
+    func addUser(user: User) {
+        savedUser.append(user)
+    }
+    
+    func removeUser(user: User){
+        guard let index = savedUser.firstIndex(of: user) else { return }
+        
+        savedUser.remove(at: index)
+        
+        
+    }
+    
+    func fetchUser(completion: @escaping (Result<User, Error>)->Void){
+        guard let requestURL = baseURL else { return }
+        
+        URLSession.shared.dataTask(with: requestURL) { (data, response, error) in
+            if let error = error {
+                print("Error fetching user")
+                completion(.failure(error))
+                return
+            }
+            guard let data = data else {
+                completion(.failure(NSError()))
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                
+                let userData = try decoder.decode(User.self, from: data)
+                completion(.success(userData))
+            }catch {
+                NSLog("Error decoding data to type User: \(error)")
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+    
+    func fetchImage(at urlString: String, completion: @escaping (UIImage?)->Void){
+        guard let url = URL(string: urlString) else {
+            completion(nil)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            if let error = error {
+                NSLog("Error fetching image: \(error)")
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned from data task")
+                completion(nil)
+                return
+            }
+            
+            let image = UIImage(data: data)
+            completion(image)
+        }.resume()
+    }
+    
+}
+
+
