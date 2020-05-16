2503927a502502882b72ba82cac597ad54b3174f
Claudia Maciel <clc_80@yahoo.com>
2020-05-15 19:01:09 -0700

    Networking calls set up

---
+++ Controller/RandomUsersController.swift
@@ -0,0 +1,69 @@
+//
+//  RandomUsersController.swift
+//  Random Users
+//
+//  Created by Claudia Contreras on 5/15/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RandomUsersController {
+    // MARK: - Properties
+    
+    var randomUsers: RandomUsersResults?
+    let baseURL = URL(string: "https://randomuser.me/api/?format=json&inc=name,email,phone,picture&results=1000")!
+    
+    
+    // MARK: - Functions
+    
+    // Get the users
+    func getUsers(completion: @escaping (RandomUsersResults?, Error?) -> Void) {
+        var request = URLRequest(url: baseURL)
+        request.httpMethod = "GET"
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { (data, _, error) in
+            guard error == nil else {
+                completion(nil, error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(nil, error)
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                self.randomUsers = try decoder.decode(RandomUsersResults.self, from: data)
+                completion(self.randomUsers, nil)
+            } catch {
+                completion(nil, error)
+                return
+            }
+        }
+        dataTask.resume()
+    }
+    
+    // Get the image
+    func downloadUserImage(path: String, completion: @escaping(Data?, Error?) -> Void ) {
+        var request = URLRequest(url: URL(string: path)!)
+        request.httpMethod = "GET"
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { (data, _, error) in
+            guard error == nil else {
+                completion(nil, error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(nil, error)
+                return
+            }
+            completion(data, nil)
+        }
+        
+        dataTask.resume()
+    }
+}
---
+++ Model/UserResults.swift
@@ -0,0 +1,30 @@
+//
+//  UserResults.swift
+//  Random Users
+//
+//  Created by Claudia Contreras on 5/15/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+struct UserResults: Codable {
+    let name: Name
+    let email: String
+    let phone: String
+    let picture: Picture
+}
+
+struct Name: Codable {
+    let title: String
+    let first: String
+    let last: String
+}
+
+struct Picture: Codable {
+    let medium: String
+}
+
+struct RandomUsersResults: Codable {
+    let user: [UserResults]
+}
