1239cbb1ce7f5bb6f2ee9f3d9cfcab2b65c30d0d
Pravin Patel <patelpra337@gmail.com>
2020-05-16 12:08:12 -0500

    Add and update new project random users

---
+++ Random Users/Models/Users.swift
@@ -0,0 +1,54 @@
+//
+//  Users.swift
+//  Random Users
+//
+//  Created by patelpra on 5/16/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+struct User: Equatable, Decodable {
+    enum Keys: String, CodingKey {
+        case name
+        case phone
+        case email
+        case picture
+        
+        enum NameKeys: String, CodingKey {
+            case first, last
+        }
+        
+        enum ImageKeys: String, CodingKey {
+            case large, thumbnail
+        }
+    }
+    
+    let name: String
+    let phone: String
+    let email: String
+    let thumbnailImage: URL
+    let largeImage: URL
+    
+    init(name: String, phone: String, email: String, thumbnailImage: URL, largeImage: URL) {
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.thumbnailImage = thumbnailImage
+        self.largeImage = largeImage
+    }
+    
+    struct Users: Decodable {
+        let results: [User]
+        
+        enum UserKeys: String, CodingKey {
+            case results
+        }
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: UserKeys.self)
+            
+            self.results = try container.decode([User].self, forKey: .results)
+        }
+    }
+}
