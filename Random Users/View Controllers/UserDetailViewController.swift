eea9fada922a7c7564b803c4c409fdfe9469f5e3
hai666l <0xconr@protonmail.com>
2020-05-18 18:48:15 -0500

    Setup Cell and Detail Controllers

--- Random Users/View Controllers/UserDetailViewController.swift
+++ Random Users/View Controllers/UserDetailViewController.swift
@@ -13,9 +13,42 @@ class UserDetailViewController: UIViewController {
     @IBOutlet weak var username: UILabel!
     @IBOutlet weak var phone: UILabel!
     @IBOutlet weak var email: UILabel!
-    
+
+    var user: User?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
     }
-
+    
+    private func updateViews() {
+        guard let user = user else { return }
+        username.text = user.name.displayName
+        phone.text = user.phone
+        email.text = user.email
+        
+        fetchImage()
+    }
+    
+    private func fetchImage() {
+        guard let user = user, let imageURL = URL(string: user.picture.large) else { return }
+        
+        URLSession.shared.dataTask(with: imageURL) { data, _, error in
+            if let error = error {
+                NSLog("Failed to fetch image: \(error)")
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("Fetched image, no data")
+                return
+            }
+            
+            let image = UIImage(data: data)
+            DispatchQueue.main.async {
+                self.image.image = image
+            }
+        }
+        .resume()
+    }
 }
