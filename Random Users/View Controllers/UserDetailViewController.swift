726c19005aa4d9e9a6e0ff32b2f085650634bfe5
hai666l <0xconr@protonmail.com>
2020-05-18 18:24:58 -0500

    Link Outlets

---
+++ Random Users/View Controllers/UserDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  UserDetailViewController.swift
+//  Random Users
+//
+//  Created by conner on 5/15/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var phone: UILabel!
+    @IBOutlet weak var email: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
