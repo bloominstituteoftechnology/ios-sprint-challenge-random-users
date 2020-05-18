251b3bf1e89af869e4229f01182ccf65970c6833
Matt Martindale <martindalematt18@yahoo.com>
2020-05-17 19:24:38 -0600

    setUp viewControllers and UI

---
+++ Random Users/View Controllers/DetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailViewController.swift
+//  Random Users
+//
+//  Created by Matthew Martindale on 5/17/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var userDetailImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
---
+++ Random Users/Views/UserTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  UserTableViewCell.swift
+//  Random Users
+//
+//  Created by Matthew Martindale on 5/17/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var userThumbnailImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+
+}
