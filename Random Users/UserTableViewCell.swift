eda05eda751a1008d8c095ef0dc3d8dd0df9e235
Thomas Dye <thomasallendye@gmail.com>
2020-05-23 10:33:32 -0400

    Created table view cell

---
+++ Random Users/UserTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  UserTableViewCell.swift
+//  Random Users
+//
+//  Created by Thomas Dye on 5/23/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: TableViewCell {
+
+}
