e37f1656c96e1268eb44f3d038ef5a8685aee320
nonye <nnezekwo@gmail.com>
2020-06-05 09:56:20 -0700

    Storyboard set up

---
+++ Random Users/Resources/PeopleCell.swift
@@ -0,0 +1,24 @@
+//
+//  PeopleCell.swift
+//  Random Users
+//
+//  Created by Nonye on 6/5/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class PeopleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
