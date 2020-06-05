13fdd4442fd73f9b64838ba7510b62a744befc57
Joe V <joeveverka89@gmail.com>
2020-06-05 11:12:01 -0400

    setup Userclient and Cache

--- Random Users/Networking/Cache.swift
+++ Random Users/Networking/Cache.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+
+typealias Bytes = Int
+
+class Cache<Key: Hashable, Value> {
+
+    init(size: Bytes) {
+        self.limit = size
+    }
+
+    //MARK: - Public Methods
+
+    func cache(_ value: Value, ofSize bytes: Bytes, for key: Key) {
+        queue.async {
+            self.store[key] = (value, bytes)
+            self.chronologicalKeys.append(key)
+
+            self.storeSize += bytes
+            if self.storeSize > self.limit {
+                self.evictOldEntries()
+            }
+        }
+
+        func value(for key: Key) -> Value? {
+            queue.sync { store[key]?.value }
+        }
+    }
+
+    //MARK: - Private
+
+    private var store: [Key: (value: Value, size: Bytes)] = [:]
+    private var chronologicalKeys: [Key] = []
+    private let queue = DispatchQueue(label: "Cache Queue")
+    private var storeSize: Bytes = 0
+    private var limit: Bytes = 0
+
+    private func evictOldEntries() {
+        while storeSize > limit / 2 {
+            guard !chronologicalKeys.isEmpty else { break }
+            let key = chronologicalKeys.removeFirst()
+            guard let value = store.removeValue(forKey: key) else { continue }
+            storeSize -= value.size
+        }
+    }
+}
--- Random Users/Networking/UserClient.swift
+++ Random Users/Networking/UserClient.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+class UserClient {
+    private let url = URL(string:  "https://randomuser.me/api/?format=json&inc=name,email,phone,picture&results=1000")!
+
+    func fetchUsers(completion: @escaping (Result<[Users], NetworkError>) -> Void) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(.clientError(error)))
+                return
+            }
+
+            if let response = response as? HTTPURLResponse,
+                !(200...299).contains(response.statusCode) {
+                completion(.failure(.invalidResponseCode(response.statusCode)))
+                return
+            }
+
+            guard let data = data else {
+                completion(.failure(.noData))
+                return
+            }
+
+            do {
+                let wrapper = try JSONDecoder().decode(UserWrapper.self, from: data)
+                let users = wrapper.results
+                completion(.success(users))
+            } catch {
+                completion(.failure(.failedDecode(error)))
+            }
+        }
+    }
+}
