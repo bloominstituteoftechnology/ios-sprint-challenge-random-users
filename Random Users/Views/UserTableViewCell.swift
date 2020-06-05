48efe52f63d45c3d87480a309bb0920ed5482b82
Cody Morley <codymorleyinc@gmail.com>
2020-06-05 15:27:48 -0700

    added UserTableViewCell to project and wired in outlets to IB.

---
+++ Random Users/Views/UserTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  UserTableViewCell.swift
+//  Random Users
+//
+//  Created by Cody Morley on 6/5/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+    //MARK: - Properties -
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+
+}
