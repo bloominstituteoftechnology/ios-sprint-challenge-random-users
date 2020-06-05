84acf56b8d2ff24e9f6068ef225a24894fe17666
Harmony Radley <harmonyradley99@gmail.com>
2020-06-05 13:25:34 -0400

    Created Files for project

---
+++ Random Users/Networking/UserController.swift
@@ -0,0 +1,9 @@
+//
+//  UserController.swift
+//  Random Users
+//
+//  Created by Harmony Radley on 6/5/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
---
+++ Random Users/URL+Secure.swift
@@ -0,0 +1,17 @@
+//
+//  URL+Secure.swift
+//  Random Users
+//
+//  Created by Harmony Radley on 6/5/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+extension URL {
+    var usingHTTPS: URL? {
+        guard var components = URLComponents(url: self, resolvingAgainstBaseURL: true) else { return nil }
+        components.scheme = "https"
+        return components.url
+    }
+}
