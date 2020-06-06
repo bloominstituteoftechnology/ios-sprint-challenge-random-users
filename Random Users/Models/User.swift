4aeb500514f43bf9cd9790648750f6fc73590009
Cody Morley <codymorleyinc@gmail.com>
2020-06-06 12:57:21 -0700

    model may be done.

--- Random Users/Models/User.swift
+++ Random Users/Models/User.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct User: Codable {
+struct User: Decodable {
     //MARK: - Types -
     enum CodingKeys: String, CodingKey {
         case name
@@ -18,8 +18,8 @@ struct User: Codable {
         
         enum NameKeys: String, CodingKey {
             case title
-            case firstName = "first"
-            case lastName = "last"
+            case first
+            case last
         }
         
         enum ImageKeys: String, CodingKey {
@@ -30,14 +30,29 @@ struct User: Codable {
     
     
     //MARK: - Properties -
-    let name: [String]
+    let name: String
     let email: String
     let phoneNumber: String
-    let picture: [String]
     let image: String
     let thumbnail: String
-    let firstName: String
-    let lastName: String
+    
+    
+    //MARK: - Initializers -
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.email = try container.decode(String.self, forKey: .email)
+        self.phoneNumber = try container.decode(String.self, forKey: .phoneNumber)
+        
+        let nameContainer = try container.nestedContainer(keyedBy: CodingKeys.NameKeys.self, forKey: .name)
+        let first = try nameContainer.decode(String.self, forKey: .first)
+        let last = try nameContainer.decode(String.self, forKey: .last)
+        self.name = "\(first) \(last)"
+        
+        let imageContainer = try container.nestedContainer(keyedBy: CodingKeys.ImageKeys.self, forKey: .picture)
+        self.thumbnail = try imageContainer.decode(String.self, forKey: .thumbnail)
+        self.image = try imageContainer.decode(String.self, forKey: .image)
+    }
     
     
 }
