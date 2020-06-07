db217a9f856fb6a2d8d85b8a115a97fb3403cb39
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-06-07 10:43:03 -0700

    All files created

---
+++ Random Users/Models/Cache.swift
@@ -0,0 +1,9 @@
+//
+//  Chache.swift
+//  Random Users
+//
+//  Created by Enzo Jimenez-Soto on 6/7/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
---
+++ Random Users/Models/RandomUserssResult.swift
@@ -0,0 +1,9 @@
+//
+//  RandomUserssResult.swift
+//  Random Users
+//
+//  Created by Enzo Jimenez-Soto on 6/7/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
