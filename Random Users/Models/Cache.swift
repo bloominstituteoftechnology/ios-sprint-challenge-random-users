d949b539434a17ffaf486133415a722abe966557
Bronson <bronsonmullens@icloud.com>
2020-07-17 19:47:27 -0700

    Set up networking functions

---
+++ Random Users/Models/Cache.swift
@@ -0,0 +1,15 @@
+//
+//  Cache.swift
+//  Random Users
+//
+//  Created by Bronson Mullens on 7/17/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+class Cache {
+    
+    // MARK: - Properties
+    
+}
--- Random Users/Views/ContactTableViewCell.swift
+++ Random Users/Views/ContactTableViewCell.swift
@@ -10,12 +10,11 @@ import UIKit
 
 class ContactTableViewCell: UITableViewCell {
     
+    // MARK: - Properties
+    
     static var reuseIdentifier = "ContactCell"
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+    var apiController: APIController?
+    
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
