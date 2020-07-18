eda569d1ecf61a09ad741aea929cf8245f427562
Bronson <bronsonmullens@icloud.com>
2020-07-17 18:58:43 -0700

    Polished views and set up cell

---
+++ Random Users/Views/ContactTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ContactTableViewCell.swift
+//  Random Users
+//
+//  Created by Bronson Mullens on 7/17/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+    
+    static var reuseIdentifier = "ContactCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
