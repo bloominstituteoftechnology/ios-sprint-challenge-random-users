e642ea46793dcf20a021274c590da740fe9ed638
davidWilliams96080 <burmesedragon@att.net>
2020-07-19 21:03:47 -0700

    wrote some custom cell info

--- Random Users/Views/CustomTableViewCell.swift
+++ Random Users/Views/CustomTableViewCell.swift
@@ -10,7 +10,19 @@ import UIKit
 
 class CustomTableViewCell: UITableViewCell {
 
+    
     @IBOutlet weak var userImage: UIImageView!
     @IBOutlet weak var userName: UILabel!
+    
+    var user: Users? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    func updateViews() {
+        guard let user = user else { return }
+        userName.text = (user.name.first + user.name.last)
+    }
     
 }
