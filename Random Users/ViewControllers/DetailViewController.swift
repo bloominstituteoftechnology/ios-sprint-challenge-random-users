d1c5dc98b2ee90ad3559b8f901347caa723d2a04
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-09-12 10:26:58 -0700

    Storyboard

---
+++ Random Users/ViewControllers/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Random Users
+//
+//  Created by Cora Jacobson on 9/12/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Random Users/Views/UserTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  UserTableViewCell.swift
+//  Random Users
+//
+//  Created by Cora Jacobson on 9/12/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
