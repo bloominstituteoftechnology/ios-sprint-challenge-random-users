6794c72baec38a2b66ad7bc7144ce4416c3a1b68
Ben James <bejjmail@gmail.com>
2020-09-13 21:03:00 -0700

    finished project

---
+++ Random Users/Resources/Cache.swift
@@ -0,0 +1,31 @@
+//
+//  Cache.swift
+//  Random Users
+//
+//  Created by Dojo on 9/13/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+class Cache<Key: Hashable, Value> {
+
+    // MARK: - Properties
+    private var dictionary: [Key : Value] = [:]
+
+    private let queue = DispatchQueue(label: "BackgroundCacheDictionaryQueue")
+
+    // MARK: - Methods
+    
+    func cache(value: Value, for key: Key) {
+        queue.async {
+            self.dictionary[key] = value
+        }
+    }
+
+    func value(for key: Key) -> Value? {
+        return queue.sync {
+            return dictionary[key]
+        }
+    }
+}
---
+++ Random Users/Resources/User.swift
@@ -0,0 +1,26 @@
+//
+//  User.swift
+//  Random Users
+//
+//  Created by Dojo on 9/13/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    var name: Name
+    var email: String
+    var phone: String
+    var picture: Picture
+}
+
+// MARK: - Nested Objects
+struct Name: Codable {
+    var title, first, last: String
+    var full: String { "\(title) \(first) \(last)" }
+}
+
+struct Picture: Codable {
+    var large, medium, thumbnail: URL
+}
