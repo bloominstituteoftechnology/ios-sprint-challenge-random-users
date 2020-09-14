4a28d59993f94dcdb3de4ab861d98bd0d218ac57
Zachary Thacker <zachthac@aol.com>
2020-09-13 22:05:56 -0400

    Set up storyboard

---
+++ Random Users/ViewControllers/ContactTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ContactTableViewCell.swift
+//  Random Users
+//
+//  Created by Zachary Thacker on 9/13/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var contactImageView: UIImageView!
+    
+    @IBOutlet weak var contactNameLabel: UILabel!
+    
+   
+
+}
