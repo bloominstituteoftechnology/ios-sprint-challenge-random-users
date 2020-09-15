d9ece0409f052b1d60c1b819806924ea442a6553
Gladymir <gaethano815@gmail.com>
2020-09-14 21:01:56 -0600

    MVP done

--- Random Users/Operations/UsersTableViewController.swift
+++ Random Users/Operations/UsersTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class UsersTableViewController: UITableViewController {
     
+
+    
    let randomUsersApiController = RandomUsersApiController()
     private let photoFetchQueue = OperationQueue()
     private let cache = Cache<String, Data>()
@@ -17,13 +19,21 @@ class UsersTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         self.randomUsersApiController.fetchRandomUserDetails {_ in
-            DispatchQueue.main.async { self.tableView.reloadData() }
+            DispatchQueue.main.async { self.tableView.reloadData()
+                
+            }
         }
 
     }
 
     // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return randomUsersApiController.users.count
